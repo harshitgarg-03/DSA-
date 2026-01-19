6bb309450837868d6de4494ebfe39cdd67a7c353
Harshit Garg <gargh2120@gmail.com>
2026-01-19 20:23:16 +0530

    calculate Trap water Between Buildings .!

--- Array/1D-Array/TrapWater.java
+++ Array/1D-Array/TrapWater.java
@@ -1,8 +1,23 @@
 import java.util.*;
 
 public class TrapWater {
+
+    public static int trappingH2O(int marks[]) {
+        int counttrapwater = 0; 
+        for (int i = 0; i < marks.length-1; i++) {
+            if(marks[i] > marks[i+1]){
+                counttrapwater += (marks[i] - marks[i+1]);
+            }else if(marks[i] < marks[i+1]){
+                counttrapwater += (marks[i+1] - marks[i]);
+            }
+        }
+        return counttrapwater;
+    }
     public static void main(String args[]) {
-        int marks[] = {};
-        
+        int marks[] = {4, 2, 0, 6, 3, 2, 5};
+
+        int TrappedWater = trappingH2O(marks);
+
+        System.out.println("Trapped water between building : "+ TrappedWater);
     }
 }
