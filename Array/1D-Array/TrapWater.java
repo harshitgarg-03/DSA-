a43f791ad47707af496df740a3d3f90badd8fb5a
Harshit Garg <gargh2120@gmail.com>
2026-01-20 22:01:39 +0530

    calculate Trap water Between Buildings .!

--- Array/1D-Array/TrapWater.java
+++ Array/1D-Array/TrapWater.java
@@ -4,17 +4,43 @@ public class TrapWater {
 
     public static int trappingH2O(int marks[]) {
         int counttrapwater = 0; 
+        int ms = 0;
+        int highidx = 0;
         for (int i = 0; i < marks.length-1; i++) {
-            if(marks[i] > marks[i+1]){
-                counttrapwater += (marks[i] - marks[i+1]);
-            }else if(marks[i] < marks[i+1]){
-                counttrapwater += (marks[i+1] - marks[i]);
+            if(ms < marks[i]){
+                ms = marks[i];
+                highidx = i;
             }
         }
+        // Left side
+        int lms = 0;
+        for (int i = 0; i < highidx; i++) {
+            lms = Math.max(lms, marks[i]);
+        }
+        int lefttrapwater = 0;
+        for (int i = 0; i < highidx; i++) {
+            lefttrapwater += (lms - marks[i]);
+        }
+        System.out.println(lefttrapwater);
+        // lefttrapwater += (ms - lms);
+        
+        // Right side
+        int rms = 0;
+        for (int i = highidx+1; i < marks.length; i++) {
+            rms = Math.max(rms, marks[i]);
+        }
+        int righttrapwater = 0;
+        for (int i = highidx+1; i < marks.length; i++) {
+            righttrapwater += (rms - marks[i]);
+        }
+        // righttrapwater += (ms - rms);
+
+        // Total 
+        counttrapwater = (righttrapwater + lefttrapwater);
         return counttrapwater;
     }
     public static void main(String args[]) {
-        int marks[] = {4, 2, 0, 6, 3, 2, 5};
+        int marks[] = {0,1,0,2,1,0,1,3,2,1,2,1};
 
         int TrappedWater = trappingH2O(marks);
 
