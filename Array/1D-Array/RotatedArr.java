84ad0084d08b79ad0689cab4b6371d4e009102da
Harshit Garg <gargh2120@gmail.com>
2026-01-21 22:50:23 +0530

    rotated array problems .!

---
+++ Array/1D-Array/RotatedArr.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class RotatedArr {
+
+    public static int SortedArray(int marks[], int target) {
+        for (int i = 0; i < marks.length; i++) {
+            if(marks[i] == target){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String args[]) {
+        int marks[] = {1};
+
+        int target = 0;
+        int result = SortedArray(marks, target);
+        System.out.println(result);
+
+    }
+}
