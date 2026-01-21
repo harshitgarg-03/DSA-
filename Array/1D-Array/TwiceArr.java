504f3724a7a31cab4c20ea62fd5c94a5949f1149
Harshit Garg <gargh2120@gmail.com>
2026-01-21 14:05:16 +0530

    calculate twice number exist in an array  .!

--- Array/1D-Array/TwiceArr.java
+++ Array/1D-Array/TwiceArr.java
@@ -5,34 +5,34 @@ public class TwiceArr {
     public static boolean checkingtwice(int clonearray[], int i) {
         int count = 0;
 
-        for (int k = i; k < clonearray.length; k++){
-            if(clonearray[k] == clonearray[i]){
+        for (int k = i; k < clonearray.length; k++) {
+            if (clonearray[k] == clonearray[i]) {
                 count++;
             }
         }
 
-        if(count > 1){
+        if (count > 1) {
             System.out.println("in this array duplicete exist.!");
             return true;
         }
         return false;
     }
+
     public static void twicefind(int marks[]) {
         int clonearray[] = marks.clone();
         for (int i = 0; i < marks.length; i++) {
             boolean result = checkingtwice(clonearray, i);
-            if(result){
+            if (result) {
                 return;
             }
         }
 
         System.out.println("no any duplicate in this arrray .!");
     }
+
     public static void main(String args[]) {
-        int marks[] = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
-
+        int marks[] = { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
         twicefind(marks);
-
-        // System.out.println("This array is showing that : " + result);
     }
+    
 }
