ac1a6715622ffd3f1c35a93e12a7947b4241c828
Harshit Garg <gargh2120@gmail.com>
2026-01-23 11:28:53 +0530

    Triplet problem .!

--- Array/1D-Array/TripletPr.java
+++ Array/1D-Array/TripletPr.java
@@ -9,28 +9,7 @@ public class TripletPr {
             System.out.println("Array length not exist for triplet..!");
             return;
         }
-        /*
-         * int Arr[] = new int[marks.length-3];
-         * int fs = marks[0];
-         * int ls = marks[marks.length-1];
-         * int k = marks.length-1;
-         * int j = 0;
-         * 
-         * while ( k >= 2) {
-         * for(int i = 1; i <= marks.length-2; i++ ){
-         * if((ls + fs + marks[i]) == 0){
-         * Arr[i] = {ls, fs, marks[i]};
-         * System.out.println(fs + " " + marks[i] + " " + ls );
-         * }
-         * }
-         * 
-         * System.out.println("ls"+ ls);
-         * System.out.println("k"+ k);
-         * k--;
-         * ls = marks[k];
-         * }
-         */
-
+        
         Arrays.sort(marks);
 
         int i = 0;
