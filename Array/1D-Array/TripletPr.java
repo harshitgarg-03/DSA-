1353dc3e66892bc07271aa41926912580d55d58e
Harshit Garg <gargh2120@gmail.com>
2026-01-23 11:10:59 +0530

    Triplet problem .!

--- Array/1D-Array/TripletPr.java
+++ Array/1D-Array/TripletPr.java
@@ -3,33 +3,58 @@ import java.util.*;
 public class TripletPr {
 
     public static void findtriplet(int marks[]) {
-        
-        if(marks.length < 3){
-            System.out.println("Element length not exist for triplet..!");
+
+        // BASE CASE...
+        if (marks.length < 3) {
+            System.out.println("Array length not exist for triplet..!");
             return;
         }
-        int Arr[] = new int[marks.length-3];
-        int fs = marks[0];
-        int ls = marks[marks.length-1];
-        int k = marks.length-1;
-        int j = 0;
-        
-        while ( k >= 2) {
-            for(int i = 1; i <= marks.length-2; i++ ){
-                if((ls + fs + marks[i]) == 0){
-                    Arr[i] = {ls, fs, marks[i]};
-                    System.out.println(fs + " " + marks[i] + " " + ls );
+        /*
+         * int Arr[] = new int[marks.length-3];
+         * int fs = marks[0];
+         * int ls = marks[marks.length-1];
+         * int k = marks.length-1;
+         * int j = 0;
+         * 
+         * while ( k >= 2) {
+         * for(int i = 1; i <= marks.length-2; i++ ){
+         * if((ls + fs + marks[i]) == 0){
+         * Arr[i] = {ls, fs, marks[i]};
+         * System.out.println(fs + " " + marks[i] + " " + ls );
+         * }
+         * }
+         * 
+         * System.out.println("ls"+ ls);
+         * System.out.println("k"+ k);
+         * k--;
+         * ls = marks[k];
+         * }
+         */
+
+        Arrays.sort(marks);
+
+        int i = 0;
+
+        ArrayList<int[]> list = new ArrayList<>();
+        // ArrayList<ArrayList<int[]>> list = new ArrayList<>();
+        while (i <= marks.length - 3) {
+            int Ft = marks[i];
+            int St = marks[i + 1];
+
+            for (int j = i + 2; j < marks.length; j++) {
+                if ((Ft + St + marks[j]) == 0) {
+                    int arr[] = { Ft, St, marks[j] };
+                    list.add(arr);
                 }
             }
-            
-            System.out.println("ls"+ ls);
-            System.out.println("k"+ k);
-            k--;
-            ls = marks[k];
+        }
+        for (int j = 0; j < list.length; j++) {
+            System.out.println(list[j]);
         }
     }
+
     public static void main(String args[]) {
-        int marks[] = {-1, 0, 1, 2, -1, -4};
+        int marks[] = { -1, 0, 1, 2, -1, -4 };
 
         findtriplet(marks);
     }
