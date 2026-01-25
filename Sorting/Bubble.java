0afc7c10d6fcbe39413d7d89b4b6d98aa815d1ea
Harshit Garg <gargh2120@gmail.com>
2026-01-25 13:41:11 +0530

    Sort an array through Bubbule.!

---
+++ Sorting/Bubble.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Bubble {
+
+    public static void bubbleSort(int arr[]) {
+        int p = 1;
+        while (p < arr.length) {
+            int didswap = 0;
+            for (int i = 0; i < arr.length - p; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    int tepm = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = tepm;
+                    didswap = 1;
+                }
+            }
+            if(didswap == 0){
+                break;
+            }
+            p++;
+        }
+        
+        for (int index = 0; index < arr.length; index++) {
+            System.out.print(arr[index] + " ");
+        }
+        
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {1, 2, 3, 4};
+        bubbleSort(arr);
+    }
+}
