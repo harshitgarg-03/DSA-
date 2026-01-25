d53dc483e42fd00cb1f271b4bf9efb37c0c588f9
Harshit Garg <gargh2120@gmail.com>
2026-01-25 21:00:05 +0530

    Sort an array through Selection Sort.!

---
+++ Sorting/Selection.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Selection {
+    public static void SelectionSort(int marks[]) {
+        int p = 1;
+       
+        while (p < marks.length) {
+            int min = marks[p];
+            int k = p;
+            for (int i = p; i < marks.length; i++) {
+                if(marks[i] < min){
+                    min = marks[i];
+                    k = i;
+                }
+            }
+            if (marks[p - 1] > min) {
+                int tepm = marks[p - 1];
+                marks[p - 1] = min;
+                marks[k] = tepm;
+            }
+            p++;
+        }
+        for (int index = 0; index < marks.length; index++) {
+            System.out.print(marks[index] + " ");
+        }
+
+    }
+
+    public static void main(String args[]) {
+        int marks[] = { 5,2,3,1 };
+        SelectionSort(marks);
+    }
+}
