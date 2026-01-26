8e0866e5346df082e65d7d738ed9e8d6039ee02d
Harshit Garg <gargh2120@gmail.com>
2026-01-26 10:27:04 +0530

    sort through Insertion sort .!

--- Sorting/Insertion.java
+++ Sorting/Insertion.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Insertion {
     public static void InsertionSort(int marks[]) {
-        
+        int p = 1;
     }
     public static void main(String args[]) {
         int marks[] = {5, 4, 1, 3, 2};
