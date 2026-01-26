9c3ef217b5df4d4102c656e5dcb3d208d38cede3
Harshit Garg <gargh2120@gmail.com>
2026-01-26 18:41:42 +0530

    sort through Counting  sort .!

--- Sorting/Counting.java
+++ Sorting/Counting.java
@@ -1,7 +1,11 @@
 import java.util.*;
 
 public class Counting {
+    public static void countingSort(int marks[]) {
+        
+    }
     public static void main(String args[]) {
-
+        int marks[] = {5, 4, 1, 3, 2};
+        countingSort(marks);
     }
 }
