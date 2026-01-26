358a6023f5b687b784640b1e15102dec196a11a3
Harshit Garg <gargh2120@gmail.com>
2026-01-26 10:24:46 +0530

    sort through Insertion sort .!

--- Sorting/Insertion.java
+++ Sorting/Insertion.java
@@ -1,6 +1,9 @@
 import java.util.*;
 
 public class Insertion {
+    public static void main(String[] args) {
+        
+    }
     public static void main(String args[]) {
         int marks[] = {5, 4, 1, 3, 2};
     }
