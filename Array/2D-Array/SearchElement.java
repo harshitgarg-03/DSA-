b02b6537090b759119651020772d620a01177f61
Harshit Garg <gargh2120@gmail.com>
2026-01-27 21:16:01 +0530

    Seraching element in sorted matrix through applying Staircase algo .!

--- Array/2D-Array/SearchElement.java
+++ Array/2D-Array/SearchElement.java
@@ -6,15 +6,25 @@ import java.util.*;
 (3) StairCase Search(o(n*m))
 */
 public class SearchElement {
-    public static void SearchingKey(int marks[][]) {
+    public static void SearchingKey(int marks[][], int target) {
         int row = 0;
-        int col = 
+        int col = marks[0].length - 1;
+
+        while (row < marks.length && col >= 0) {
+            if (marks[row][col] == target) {
+                System.out.println("Key Found at " + row + " , " + col);
+            }
+
+            if ((marks[row][col - 1] < target || marks[row - 1][col] < target)) {
+
+            }
+        }
     }
 
     public static void main(String args[]) {
-        int marks[][] = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+        int marks[][] = { { 1, 3, 5, 7 }, { 10, 11, 16, 20 }, { 23, 30, 34, 60 } };
         int target = 3;
+        SearchingKey(marks, target);
 
-        
     }
 }
