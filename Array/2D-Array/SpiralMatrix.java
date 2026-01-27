c7d1d7d79c25b678c0f4fb2086baf8d5e3a97334
Harshit Garg <gargh2120@gmail.com>
2026-01-27 12:25:53 +0530

    Printing element of a matrix in spiral fashion .!

--- Array/2D-Array/SpiralMatrix.java
+++ Array/2D-Array/SpiralMatrix.java
@@ -6,37 +6,40 @@ public class SpiralMatrix {
     public static void SpiralElements(int marks[][]) {
         int startrow = 0;
         int startcol = 0;
-        int endrow = marks.length-1;
-        int endcol = marks[0].length-1;
+        int endrow = marks.length - 1;
+        int endcol = marks[0].length - 1;
 
-        while(startrow <= endrow && startcol <= endcol){
+        while (startrow <= endrow && startcol <= endcol) {
             // top
-            for (int i = startcol; i <= endcol; i++){
+            for (int i = startcol; i <= endcol; i++) {
                 System.out.print(marks[startrow][i] + " ");
             }
             // right
-            for (int i = startrow+1; i <= endrow; i++){
+            for (int i = startrow + 1; i <= endrow; i++) {
                 System.out.print(marks[i][endcol] + " ");
             }
             // bottom
-            for (int i = endcol-1; i >= startcol; i--){
+            for (int i = endcol - 1; i >= startcol; i--) {
                 System.out.print(marks[endrow][i] + " ");
             }
             // left
-            for (int i = endrow-1; i >= startrow+1; i--){
+            for (int i = endrow - 1; i >= startrow + 1; i--) {
                 System.out.print(marks[i][startcol] + " ");
             }
 
             startrow++;
             startcol++;
             endrow--;
-            endcol--; 
+            endcol--;
 
         }
     }
 
     public static void main(String args[]) {
-        int marks[][] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 1, 2, 3 }, { 4, 5, 6, 7 } };
+        int marks[][] = { { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 },
+                { 13, 14, 15, 16 } };
         SpiralElements(marks);
     }
 }
