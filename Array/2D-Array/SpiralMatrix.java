a5953c78ff683bca006cd24682722b3584e9bb3a
Harshit Garg <gargh2120@gmail.com>
2026-01-27 11:37:30 +0530

    Printing element of a matrix in spiral fashion .!

--- Array/2D-Array/SpiralMatrix.java
+++ Array/2D-Array/SpiralMatrix.java
@@ -1,7 +1,13 @@
 import java.util.*;
 
-public class SpiralMatrix {
-    public static void main(String args[]) {
+// Printing elements in spiral fashion (outer to iner boundary)
 
+public class SpiralMatrix {
+    public static void SpiralElements(int marks[][]) {
+        
+    }
+    public static void main(String args[]) {
+        int marks[][] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 1, 2, 3 }, { 4, 5, 6, 7 } };
+        SpiralElements(marks);
     }
 }
