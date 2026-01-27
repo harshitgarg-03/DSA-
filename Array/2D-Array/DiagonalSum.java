57811312b58d79d54fb6d5935b2f16017c20d82b
Harshit Garg <gargh2120@gmail.com>
2026-01-27 14:43:21 +0530

    sum of the diagonal elements .!

--- Array/2D-Array/DiagonalSum.java
+++ Array/2D-Array/DiagonalSum.java
@@ -8,25 +8,29 @@ public class DiagonalSum {
         int row = 0;
         int col = 0;
 
-        for(int i = 0; i < marks.length; i++){
+        for (int i = 0; i < marks.length; i++) {
             DiaSum += marks[row++][col++];
-            
+
         }
-        
         // To calculate Secondary Diagonal..
 
         row = 0;
-        col = marks[0].length-1;
-        for(int i = 0; i < marks.length; i++){
-            if(row == col) continue;
+        col = marks[0].length - 1;
+        for (int i = 0; i < marks.length; i++) {
+            if (row == col) {
+                row++;
+                col--;
+                continue;
+            }
             DiaSum += marks[row++][col--];
-            
+
         }
 
         System.out.println(DiaSum);
     }
+
     public static void main(String args[]) {
-        int marks[][] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 1, 2, 3, 4 } };
-            DiagonalAdd(marks);
+        int marks[][] = { { 1, 2, 3 }, { 5, 6, 7 }, { 9, 10, 11 } };
+        DiagonalAdd(marks);
     }
 }
