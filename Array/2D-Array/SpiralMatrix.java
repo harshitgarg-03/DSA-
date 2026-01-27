98d4ab4c8102e3fb5646086a72bb4f73727fc2ed
Harshit Garg <gargh2120@gmail.com>
2026-01-27 12:17:33 +0530

    Printing element of a matrix in spiral fashion .!

--- Array/2D-Array/SpiralMatrix.java
+++ Array/2D-Array/SpiralMatrix.java
@@ -11,8 +11,30 @@ public class SpiralMatrix {
 
         while(startrow < endrow && startcol < endcol){
             // top
+            for (int i = startcol; i < endcol; i++){
+                System.out.print(marks[startrow][i] + " ");
+            }
+            // right
+            for (int i = startrow+1; i < endrow; i++){
+                System.out.print(marks[i][endcol] + " ");
+            }
+            // bottom
+            for (int i = endcol-1; i >= startcol; i--){
+                System.out.print(marks[endrow][i] + " ");
+            }
+            // left
+            for (int i = endrow-1; i >= startcol+1; i--){
+                System.out.print(marks[endrow][startcol] + " ");
+            }
+
+            startrow++;
+            startcol++;
+            endrow--;
+            endcol--; 
+
         }
     }
+
     public static void main(String args[]) {
         int marks[][] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 1, 2, 3 }, { 4, 5, 6, 7 } };
         SpiralElements(marks);
