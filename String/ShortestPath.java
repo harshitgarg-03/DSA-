5e6a964cce0d0ed159e7265b8cae03ebf04a8071
Harshit Garg <gargh2120@gmail.com>
2026-01-28 14:24:10 +0530

    Finding out Shortest path to reach at position .!

--- String/ShortestPath.java
+++ String/ShortestPath.java
@@ -9,8 +9,27 @@ public class ShortestPath {
         int y = 0;
 
         for (int index = 0; index < str.length(); index++) {
-            
+            if(str.charAt(index) == 'N'){
+                y++;
+            }
+            else if(str.charAt(index) == 'S'){
+                y--;
+            }
+            else if(str.charAt(index) == 'E'){
+                x++;
+            }
+            else if(str.charAt(index) == 'W'){
+                x--;
+            }
+
         }
+        double xdirsum = Math.pow((x-x1), 2);
+        double ydirsum = Math.pow((y-y1), 2);
+
+        double Shortpath = Math.sqrt(xdirsum+ydirsum);
+
+        System.out.println("x : " + x + "  , Y : " + y);
+        System.out.println("Shortest path is : " + Shortpath);
     }
     public static void main(String args[]) {
         String str = "WNEENESENNN";
