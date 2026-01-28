31ed66aef5bfe03192b216ecb55092119ddeb6f2
Harshit Garg <gargh2120@gmail.com>
2026-01-28 14:13:00 +0530

    Finding out Shortest path to reach at position .!

--- String/ShortestPath.java
+++ String/ShortestPath.java
@@ -2,9 +2,19 @@ import java.util.*;
 
 public class ShortestPath {
     public static void ShortestPath(String str) {
-        
+        int x1 = 0;
+        int y1 = 0;
+
+        int x = 0;
+        int y = 0;
+
+        for (int index = 0; index < str.length(); index++) {
+            
+        }
     }
     public static void main(String args[]) {
-        
+        String str = "WNEENESENNN";
+
+        ShortestPath(str);
     }
 }
