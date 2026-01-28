5cca4733790dcee04db1c17adab9b40ebf36b4bc
Harshit Garg <gargh2120@gmail.com>
2026-01-28 10:29:50 +0530

    Transpose of Original Matrix .!

--- Array/2D-Array/TransposeMatrix.java
+++ Array/2D-Array/TransposeMatrix.java
@@ -6,6 +6,7 @@ public class TransposeMatrix {
         int n = marks.length;
         int m = marks[0].length;
 
+        // Square Matrix then TC(O(N2)) and SC(O(1))
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 System.out.print(marks[j][i] + " ");
