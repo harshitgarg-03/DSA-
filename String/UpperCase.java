41097e7cd660298ef89e7855e13038a91f41c331
Harshit Garg <gargh2120@gmail.com>
2026-01-28 19:37:55 +0530

    Convert the First letter of Word in Uppercase  .!

--- String/UpperCase.java
+++ String/UpperCase.java
@@ -1,11 +1,22 @@
 import java.util.*;
 
 public class UpperCase {
+
+    public static void convertUppercase(String str) {
+        // Splitting
+        String arr[] = str.split(" ");
+
+        // Traversing
+        for (String word : arr) {
+            char oldch = word.charAt(0);
+            char newch = Character.toUpperCase(oldch);
+            word.replace(oldch, newch);
+        }
+    }
+
     public static void main(String args[]) {
         String str = "i am a harshit garg ";
 
-        for (int i = 0; i < str.length(); i++){
-            System.out.print(str.charAt(i).Upper + " ");
-        }
+        convertUppercase(str);
     }
 }
