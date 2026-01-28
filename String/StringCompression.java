c49de1d4bdbe0b57e0473c4caf5ab63ff1e3ccdc
Harshit Garg <gargh2120@gmail.com>
2026-01-28 21:35:16 +0530

    Compress the string when  repeated elements occur .!

--- String/StringCompression.java
+++ String/StringCompression.java
@@ -2,6 +2,22 @@ import java.util.*;
 
 public class StringCompression {
     public static void main(String args[]) {
+        String str[] = {"a","a","b","b","c","c","c"};
 
+        ArrayList<String> newstr = new ArrayList<>();
+        Integer count = 1;
+        for (int i =0; i < str.length-1; i++){
+            if(str[i] != str[i+1]){
+                if( count == 1){
+                    newstr.add(str[i]);
+                } else {
+                    newstr.add(str[i]);
+                    newstr.add(count.toString());
+                    count = 1;
+                }
+            } else {
+                count++;
+            }
+        }
     }
 }
