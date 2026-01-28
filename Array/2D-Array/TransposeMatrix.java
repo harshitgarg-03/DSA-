c4c3de2c76b394c3e02d8834dd328291dc6eb046
Harshit Garg <gargh2120@gmail.com>
2026-01-28 10:14:11 +0530

    Transpose of Original Matrix .!

--- Array/2D-Array/TransposeMatrix.java
+++ Array/2D-Array/TransposeMatrix.java
@@ -1,6 +1,7 @@
 import java.util.*;
-public class  TransposeMatrix  {
-public static void main(String args[]) {
 
-}
+public class TransposeMatrix {
+    public static void main(String args[]) {
+
+    }
 }
