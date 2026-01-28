03d80b43f57736c7a698fb9c0216a85401cdd4b8
Harshit Garg <gargh2120@gmail.com>
2026-01-28 15:07:24 +0530

    Finding out largest string in lexicographic manner .!

--- String/PrintLargestString.java
+++ String/PrintLargestString.java
@@ -2,6 +2,14 @@ import java.util.*;
 
 public class PrintLargestString {
     public static void main(String args[]) {
+        String str[] = {"apple", "banana", "mango"};
 
+        String lagrgest = str[0];
+
+        for (int index = 1; index < str.length; index++) {
+            if(lagrgest.compareTo(str[index]) > 0){
+                lagrgest = str[index];
+            }
+        }
     }
 }
