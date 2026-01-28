469482e31e34801f55a05815388cf464e4e01df1
Harshit Garg <gargh2120@gmail.com>
2026-01-28 10:28:30 +0530

    Transpose of Original Matrix .!

--- Array/2D-Array/TransposeMatrix.java
+++ Array/2D-Array/TransposeMatrix.java
@@ -2,6 +2,17 @@ import java.util.*;
 
 public class TransposeMatrix {
     public static void main(String args[]) {
+        int marks[][] = {{1,2,3},{4,5,6}};
+        int n = marks.length;
+        int m = marks[0].length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(marks[j][i] + " ");
+            }
+            System.out.println("");
+        }
+
 
     }
 }
