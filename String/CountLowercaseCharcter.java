c2d383b47c38f3fd7d26c2db27b9d9254698d3a4
Harshit Garg <gargh2120@gmail.com>
2026-01-29 11:26:25 +0530

    Counting the lowercase elements in a string .!

--- String/CountLowercaseCharcter.java
+++ String/CountLowercaseCharcter.java
@@ -2,6 +2,16 @@ import java.util.*;
 
 public class CountLowercaseCharcter {
     public static void main(String args[]) {
+        String str = "I am Harshit Garg";
+        int count = 0;
 
+        for ( int i = 0; i < str.length(); i++){
+
+            if(str.charAt(i) >= 97){
+                count++;
+            }
+        }
+        System.out.println(count);
+        System.out.println((str.charAt(0)));
     }
 }
