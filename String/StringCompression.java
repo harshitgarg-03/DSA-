8c607ab949845b991f911e0a58c5fc75e321a528
Harshit Garg <gargh2120@gmail.com>
2026-01-29 10:41:31 +0530

    Compress the string when  repeated elements occur .!

--- String/StringCompression.java
+++ String/StringCompression.java
@@ -1,5 +1,7 @@
 import java.util.*;
 
+// A case skipped that if count > 10 then its stored by char by char do it later 
+
 public class StringCompression {
     public static void main(String args[]) {
         String str[] = {"a","b","b","b","b","b","b","b","b","b","b","b","b"};
@@ -11,29 +13,32 @@ public class StringCompression {
             System.out.println(str[0]);
             return;
         }
+        
+        newstr.add(str[0]);
+        for (int i = 0; i < str.length-1; i++){
 
-        for (int i = 0; i < str.length - 1; i++) {
-            if (str[i] != str[i + 1]) {
-                if (count == 1) {
-                    newstr.add(str[i]);
-                } else {
-                    newstr.add(str[i]);
+            if(str[i] != str[i+1]){
+                if(count > 1){
                     newstr.add(String.valueOf(count));
                     count = 1;
                 }
+                newstr.add(str[i+1]);
             }
 
-            else if ((i == str.length - 2) && (str[i] == str[i + 1])) {
-                newstr.add(str[i]);
-                newstr.add((++count).toString());
-            } else {
+            else if ((i == str.length-2 )&& (str[i] == str[i+1])){
+                newstr.add(String.valueOf(++count));
+            }
+
+            else {
                 count++;
             }
         }
 
-        for (int i = 0; i < newstr.size(); i++) {
+        // To print new array 
+        for (int i = 0; i < newstr.size(); i++){
             System.out.print(newstr.get(i) + " ");
         }
+        
         System.out.println();
         System.out.println("Size of a list is : " + newstr.size());
     }
