01d8a99bff281598f07149d3babb1274f148c541
Harshit Garg <gargh2120@gmail.com>
2026-01-30 16:42:30 +0530

    Determining the concept of power in 2 or not .!

--- BitsManipulation.java/Power2OrNot.java
+++ BitsManipulation.java/Power2OrNot.java
@@ -1,7 +1,12 @@
-import java.util.*;
 
 public class Power2OrNot {
     public static void main(String args[]) {
+        int n = 16;
 
+        if((n&n-1) == 0){
+            System.out.println("it's a number is power of 2");
+        } else {
+            System.out.println("Number is not a power of 2");
+        }
     }
 }
