de30b72663d24bc01c435705a7eba8f96c731392
Harshit Garg <gargh2120@gmail.com>
2026-01-30 11:00:51 +0530

    Checking the odd or even number .!

--- BitsManipulation.java/OddOrEven.java
+++ BitsManipulation.java/OddOrEven.java
@@ -2,6 +2,13 @@ import java.util.*;
 
 public class OddOrEven {
     public static void main(String args[]) {
+        int BitMask = 1;
+        int n = 0;
 
+        if((n & BitMask) == 0){
+            System.out.println("It's an even number ");
+        } else {
+            System.out.println("It's an odd number ");
+        }
     }
 }
