3a63318449a5276a34e86e5ee1712b577675e0b8
Harshit Garg <gargh2120@gmail.com>
2026-01-30 19:09:07 +0530

    Fast exponent with less Time Complexity .!

--- BitsManipulation/FastExponent.java
+++ BitsManipulation/FastExponent.java
@@ -1,5 +1,22 @@
 public class FastExponent {
-    public static void main(String args[]) {
+// o(logn) in this pow convert binary number 
+    public static void CalculateExponent(int num, int pow) {
+        int ans = 1;
+        while (pow != 0){
+            int n = pow & (1 << 0);
+            if( n == 1){
+                ans = ans* num;
+            }
 
+            num = num*num;
+          pow = pow >> 1;
+        }
+        System.out.println("the power of thr number is " + ans);
+    }
+    public static void main(String args[]) {
+        int num = 2;
+        int pow = 3;
+
+        CalculateExponent(num, pow);
     }
 }
