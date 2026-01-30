95db71ea638d4da6eff26825b0f8ab4de07187ca
Harshit Garg <gargh2120@gmail.com>
2026-01-30 17:05:06 +0530

    Counting the number of set bits along with a number .!

--- BitsManipulation/SetCount.java
+++ BitsManipulation/SetCount.java
@@ -2,6 +2,17 @@ import java.util.*;
 
 public class SetCount {
     public static void main(String args[]) {
+        int n = 1;
+        int count = 0;
+        while (n != 0) {
+            int bit = n & (1 << 0);
+            if (bit == 1) {
+                count++;
+            }
 
+            n = (n >> 1);
+        }
+
+        System.out.println("Total set bits are : " + count);
     }
 }
