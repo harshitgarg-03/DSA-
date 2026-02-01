dade09938b69c94d70c61696a9d8c5b35a016aa2
Harshit Garg <gargh2120@gmail.com>
2026-02-01 19:35:46 +0530

    Find out the majority elem !

--- Practice-Sheet/Array/1DArr/MajorityElem.java
+++ Practice-Sheet/Array/1DArr/MajorityElem.java
@@ -1,7 +1,29 @@
 import java.util.*;
 
 public class MajorityElem {
-    public static void main(String args[]) {
+    public static int main(String args[]) {
+        int nums[] = { 1, 1, 2 };
+        int n = nums.length;
+        if (n == 1 || n == 2) {
+            return nums[0];
+        }
+        // 1 approach
+        // int count = 1;
+        // int maj = nums[0];
+        // for (int i = 1; i < n; i++){
+        // if(count > n/2){
+        // break;
+        // }
+        // else if(maj == nums[i]){
+        // count++;
+        // }
+        // else {
+        // count = 1;
+        // maj = nums[i];
+        // }
+        // }
 
+        Arrays.sort(nums);
+        return nums[n / 2];
     }
 }
