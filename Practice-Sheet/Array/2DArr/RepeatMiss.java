d8c95a23428d5e784fcb59ffc0a423275d7019d5
Harshit Garg <gargh2120@gmail.com>
2026-02-01 20:29:33 +0530

    Find out the Repeating and Majority Element !

--- Practice-Sheet/Array/2DArr/RepeatMiss.java
+++ Practice-Sheet/Array/2DArr/RepeatMiss.java
@@ -13,9 +13,22 @@ public class RepeatMiss {
             }
         }
 
-        for (int index = 0; index < nums.length; index++) {
-            System.out.print(nums[index] + " ");
+        Arrays.sort(nums);
+        int count = 0;
+        for (int index = 0; index < nums.length-1; index++) {
+            if((nums[index]^nums[index+1]) == 0){
+                nums[index] = 0;
+                System.out.println("Repeating number is : " + nums[index+1]);
+            }
+
+            count += nums[index];
+
         }
+        count += nums[nums.length-1];
+        int TotalSum = nums.length*(nums.length+1)/2;
+        System.out.println(TotalSum);
+        int missingNum = Math.abs(TotalSum - count);
+        System.out.print("Missing Number is : " + (missingNum));
     }
     public static void main(String args[]) {
         int marks[][] = {{1,3},{2,2}};
