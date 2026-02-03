9ed3468562b3370411e572ae8e78306c1e317c2a
Harshit Garg <gargh2120@gmail.com>
2026-02-03 12:27:20 +0530

    to maximize the profit  .!

---
+++ Practice-Sheet/Array/1DArr/BuySell.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class BuySell {
+    public static int ProfitingCalculate(int marks[]) {
+        int mp = 0;
+        int Bp = marks[0];
+        int profit = 0;
+        for(int i = 1; i < marks.length; i++){
+            if(Bp < marks[i]){
+                profit = marks[i] - Bp;
+                mp = Math.max(mp, profit);
+            } else {
+                Bp = marks[i];
+            }
+        }
+
+        return mp;
+    }
+    public static void main(String args[]) {
+        int marks[] = {7,6,4,3,1};
+
+        int maxprofit = ProfitingCalculate(marks);
+
+        System.out.println("Maximum Profit is : " + maxprofit);
+    }
+}
