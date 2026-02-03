7311701bd39d864551d33c18e0f709f77361ff88
Harshit Garg <gargh2120@gmail.com>
2026-02-03 14:26:39 +0530

    Merge two sorted arrays .!

--- Practice-Sheet/Array/1DArr/Merge2Arr.java
+++ Practice-Sheet/Array/1DArr/Merge2Arr.java
@@ -1,7 +1,27 @@
 import java.util.*;
 
 public class Merge2Arr {
-    public static void main(String args[]) {
+    public static int[] main(String args[]) {
+        int nums1[] = {1,2,3,0,0,0};
+        int nums2[] = {2,5,6};
+        int m = 3;
+        int n = 3;
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
 
+        while (j >= 0 && i >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+
+        while(j >= 0){
+                nums1[j] = nums2[j];
+                j--;
+            }
+        return nums1;
     }
 }
