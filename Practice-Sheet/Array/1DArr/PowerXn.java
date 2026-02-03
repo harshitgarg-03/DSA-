3713da63d378e31387620a7ca7da664875df8ba0
Harshit Garg <gargh2120@gmail.com>
2026-02-03 13:22:33 +0530

    Finding the power x to n throught bit manipulation .!

--- Practice-Sheet/Array/1DArr/PowerXn.java
+++ Practice-Sheet/Array/1DArr/PowerXn.java
@@ -2,6 +2,22 @@ import java.util.*;
 
 public class PowerXn {
     public static void main(String args[]) {
+        int x = 2;
+        int n = 2;
+        int pow = Math.abs(n);
+        double ans = 1;
+        while (pow != 0) {
+            if ((pow & 1) == 1) {
+                ans *= x;
+            }
 
+            x = x * x;
+            pow = pow >> 1;
+        }
+        if (n < 0) {
+            System.out.println(1 / ans);
+        } else {
+            System.out.println(ans);
+        }
     }
 }
