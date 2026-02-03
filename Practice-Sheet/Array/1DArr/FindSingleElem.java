7980c3f0093b42b1f9417abb749b83f588d2f134
Harshit Garg <gargh2120@gmail.com>
2026-02-03 12:18:50 +0530

    find single element  .!

--- Practice-Sheet/Array/1DArr/FindSingleElem.java
+++ Practice-Sheet/Array/1DArr/FindSingleElem.java
@@ -1,6 +1,21 @@
 import java.util.*;
-public class FindSingleElem {
-public static void main(String args[]) {
 
-}
+public class FindSingleElem {
+
+    public static int singleNumber(int[] nums) {
+        int singlenum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+
+            singlenum = singlenum ^ nums[i];
+
+        }
+
+        return singlenum;
+    }
+
+    public static void main(String args[]) {
+        int nums[] = { 1, 2, 2 };
+        int single = singleNumber(nums);
+        System.out.println("single number is " + single);
+    }
 }
