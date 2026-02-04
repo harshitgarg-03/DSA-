e2645831b2b37f32c128bb7e9b084d4633aa3cb9
Harshit Garg <gargh2120@gmail.com>
2026-02-04 14:35:29 +0530

    Four sum of the elements ..

--- Practice-Sheet/Array/1DArr/FourSum.java
+++ Practice-Sheet/Array/1DArr/FourSum.java
@@ -3,44 +3,62 @@ import java.util.*;
 public class FourSum {
 
     public static void GettingSum(int nums[], int target) {
-        int i = 0;
-        int j = nums.length - 1;
-        List<List<Integer>> list = new ArrayList<>();
-        Arrays.sort(nums);
-        while (i <  nums.length-3) {
-            int left = i + 1;
-            int right = j - 1;
+
+    List<List<Integer>> list = new ArrayList<>();
+
+    Arrays.sort(nums);
+
+    int n = nums.length;
+
+    for (int i = 0; i < n - 3; i++) {
+
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+        for (int j = i + 1; j < n - 2; j++) {
+
+            if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+            int left = j + 1;
+            int right = n - 1;
 
             while (left < right) {
-                int sum = (nums[i] + nums[left] + nums[right]+ nums[j]);
+
+                int sum = nums[i] + nums[j] + nums[left] + nums[right];
 
                 if (sum == target) {
-                    list.add(Arrays.asList(nums[i], nums[left], nums[right], nums[j]));
+
+                    list.add(Arrays.asList(
+                            nums[i],
+                            nums[j],
+                            nums[left],
+                            nums[right]
+                    ));
+
                     left++;
                     right--;
+
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+
                 }
 
-                else if (sum > target) {
-                    right--;
+                else if (sum < target) {
+                    left++;
                 }
 
                 else {
-                    left++;
+                    right--;
                 }
             }
-            i++;
-            j--;
         }
-    
-        Set<List<Integer>> set = new LinkedHashSet<>(list);
-
-        List<List<Integer>> UniqueList = new ArrayList<>(set);
-
-        System.out.println(UniqueList);
     }
 
+    System.out.println(list);
+}
+
     public static void main(String args[]) {
-        int nums[] = {-3,-1,0,2,4,5};
+        int nums[] = { -3,-1,0,2,4,5 };
         int target = 0;
 
         GettingSum(nums, target);
