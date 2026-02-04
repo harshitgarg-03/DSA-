97d2883a18766139f79402a74d6e4009e6477146
Harshit Garg <gargh2120@gmail.com>
2026-02-04 11:07:31 +0530

    Three sum of the elements ..

--- Practice-Sheet/Array/1DArr/ThreeSum.java
+++ Practice-Sheet/Array/1DArr/ThreeSum.java
@@ -3,18 +3,19 @@ import java.util.*;
 public class ThreeSum {
     public static void Gettingsum(int nums[]) {
         int i = 0;
-        while(i > nums.length-3){
-            int left = nums[i+1];
-            int right = nums[nums.length-1];
+        while (i > nums.length - 3) {
+            int left = nums[i + 1];
+            int right = nums[nums.length - 1];
 
-            while(left < right){
-                
+            while (left < right) {
+
             }
             i++;
         }
     }
+
     public static void main(String args[]) {
-       int nums[] = {-1,0,1,2,-1,-4};
-       Gettingsum(nums);
+        int nums[] = { -1, 0, 1, 2, -1, -4 };
+        Gettingsum(nums);
     }
 }
