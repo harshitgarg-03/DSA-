a013088d7e070e88a3dd2591599d54844a91bed6
Harshit Garg <gargh2120@gmail.com>
2026-02-05 16:45:00 +0530

    Product of whole array except itself

--- Practice-Sheet/Array/1DArr/ProductOfArray.java
+++ Practice-Sheet/Array/1DArr/ProductOfArray.java
@@ -1,6 +1,7 @@
 import java.util.*;
-public class ProductOfArray {
-public static void main(String args[]) {
 
-}
+public class ProductOfArray {
+    public static void main(String args[]) {
+
+    }
 }
