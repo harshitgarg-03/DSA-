30902e4e19b79bd57770fc8bda1c9c5b342fb371
Harshit Garg <gargh2120@gmail.com>
2026-02-05 12:08:51 +0530

    Finding out maximum water stores between two buildings

--- Practice-Sheet/Array/1DArr/ContainWithMostWater.java
+++ Practice-Sheet/Array/1DArr/ContainWithMostWater.java
@@ -1,6 +1,7 @@
 import java.util.*;
-public class ContainWithMostWater {
-public static void main(String args[]) {
 
-}
+public class ContainWithMostWater {
+    public static void main(String args[]) {
+
+    }
 }
