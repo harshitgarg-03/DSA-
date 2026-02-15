c090425207f2be1a5b00b442f1bba010a264c08d
Harshit Garg <gargh2120@gmail.com>
2026-02-15 22:16:07 +0530

    Factorial find out the number

--- Recursion/Factorialnum.java
+++ Recursion/Factorialnum.java
@@ -1,6 +1,7 @@
 import java.util.*;
-public class  Factorialnum{
-public static void main(String args[]) {
 
-}
+public class Factorialnum {
+    public static void main(String args[]) {
+
+    }
 }
