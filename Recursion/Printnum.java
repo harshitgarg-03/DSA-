a8dbef4d6bfeefe04339cced78297225c6d13ca7
Harshit Garg <gargh2120@gmail.com>
2026-02-15 20:45:38 +0530

    printing number using recursion

--- Recursion/Printnum.java
+++ Recursion/Printnum.java
@@ -1,7 +1,15 @@
 import java.util.*;
 
 public class Printnum {
-    public static void main(String args[]) {
+    public static int Printingnum(int num) {
+        if(num == 0){
+            return sum; 
+        }
 
+        int sum = num + Printingnum(num -1);
+    }
+    public static void main(String args[]) {
+        int num = 10;
+        Printingnum(10);
     }
 }
