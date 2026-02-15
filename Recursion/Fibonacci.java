1741c62e3012b5bc610e093b9fc3f5c566877d5f
Harshit Garg <gargh2120@gmail.com>
2026-02-15 23:12:43 +0530

    Fibonacci series of n terms!

--- Recursion/Fibonacci.java
+++ Recursion/Fibonacci.java
@@ -1,7 +1,16 @@
 import java.util.*;
 
 public class Fibonacci {
-    public static void main(String args[]) {
+    public static int FibSeries(int term) {
+        if( term == 0){
+            return 0;
+        }
 
+        return (FibSeries(term-1) + FibSeries(term-2));
+    }
+    public static void main(String args[]) {
+        int n = 5;
+        int sum = FibSeries(n);
+        System.out.println(sum);
     }
 }
