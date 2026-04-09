7cea4efd28f4f6eab7d6db0100d21ebaff65e4fe
Harshit Garg <gargh2120@gmail.com>
2026-04-09 17:29:24 +0530

    problem adding increasing side

--- Recursion/Printnumbers.java
+++ Recursion/Printnumbers.java
@@ -1,13 +1,25 @@
 
 public class Printnumbers {
+
+
+    // INCREASING ORDER
     public static void printnum(int num) {
         if (num == 0){
             return;
         }
-
-        System.out.println(num + " ");
+        
         printnum(num - 1);
+        System.out.println(num + " ");
     }
+    
+    // DECREASING ORDER
+    // public static void printnum(int num) {
+    //     if (num == 0){
+    //         return;
+    //     }
+    //     System.out.println(num + " ");
+    //     printnum(num - 1);
+    // }
     public static void main(String args[]) {
         int num = 10;
         printnum(num);
