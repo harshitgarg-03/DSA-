de2729b067cf0578db24397ed3163d974e875777
Harshit Garg <gargh2120@gmail.com>
2026-04-10 13:34:09 +0530

    TilingProblem defined

--- Recursion/TilingProblem.java
+++ Recursion/TilingProblem.java
@@ -1,5 +1,18 @@
 public class TilingProblem {
-    public static void main(String args[]) {
+    public static int CalculateWays(int num) {
+        if(num == 0 || num == 1){
+            return 1;
+        }
+        int verticalways = CalculateWays(num - 1);
+        int horizontalalways = CalculateWays(num - 2);
 
+        int ways = (verticalways + horizontalalways);
+        return ways;
+    }
+
+    public static void main(String args[]) {
+        int n = 5;
+        int ways = CalculateWays(n);
+        System.out.println("Ways are " + ways);
     }
 }
