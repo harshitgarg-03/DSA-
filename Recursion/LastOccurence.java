e2746e4365d4e5c301f0f58fe9a9d0564041b0fd
Harshit Garg <gargh2120@gmail.com>
2026-04-10 09:53:49 +0530

    Checking last occurence element in an array

--- Recursion/LastOccurence.java
+++ Recursion/LastOccurence.java
@@ -1,12 +1,12 @@
 
 public class LastOccurence {
     public static void LastOccur(int arr[], int key, int i) {
-        if(i == arr.length-1){
+        if (i == arr.length - 1) {
             System.out.println("Key not found !");
             return;
         }
-        LastOccur(arr, key, i+1);
-        if(arr[i] == key){
+        LastOccur(arr, key, i + 1);
+        if (arr[i] == key) {
             System.out.println("Key's first occurence is index " + i);
             return;
         }
@@ -15,7 +15,7 @@ public class LastOccurence {
 
     public static void main(String args[]) {
         int key = 7;
-        int arr[] = {1, 5, 3, 4, 5, 5, 7, 5};
+        int arr[] = { 1, 5, 3, 4, 5, 5, 7, 5 };
         LastOccur(arr, key, 0);
     }
 }
