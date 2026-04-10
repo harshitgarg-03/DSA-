0c4c9072d7e9f5cbcd33135c14baaa7091cb0083
Harshit Garg <gargh2120@gmail.com>
2026-04-10 12:10:56 +0530

    power x to n

--- Recursion/PowerXN.java
+++ Recursion/PowerXN.java
@@ -1,6 +1,15 @@
 
 public class PowerXN {
+    public static int CalculatePower(int x, int n) {
+        if(n == 0){
+            return 1;
+        }
+        return x * CalculatePower(x, n-1);
+    }
     public static void main(String args[]) {
-
+        int x = 3;
+        int n = 5;
+        int num = CalculatePower(x, n);
+        System.out.println("pwer is " + num);
     }
 }
