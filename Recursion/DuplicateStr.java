974a2dded0fdd45f0053f0e527e0b16b83cd546e
Harshit Garg <gargh2120@gmail.com>
2026-04-10 17:30:02 +0530

    Remove duplicate string

--- Recursion/DuplicateStr.java
+++ Recursion/DuplicateStr.java
@@ -1,6 +1,22 @@
 
 public class DuplicateStr {
-    public static void main(String args[]) {
+    public static void UniquePtr(String str, int i, StringBuilder newstr) {
+        if (i == str.length()) {
+            System.out.println("Original string is " + newstr);
+            return;
+        }
+        char ch = str.charAt(i);
 
+        if (newstr.indexOf(String.valueOf(ch)) == -1) {
+            newstr.append(ch);
+        }
+        UniquePtr(str, i + 1, newstr);
+
+    }
+
+    public static void main(String args[]) {
+        String str = "appnnacollege";
+
+        UniquePtr(str, 0, new StringBuilder());
     }
 }
