7df261d0fa3c5055bb8f51351563898e3f8333b7
Harshit Garg <gargh2120@gmail.com>
2026-04-10 12:34:24 +0530

    power x to n optimized

--- Recursion/OptimizedPowerXtoN.java
+++ Recursion/OptimizedPowerXtoN.java
@@ -1,6 +1,21 @@
 
 public class OptimizedPowerXtoN {
-    public static void main(String args[]) {
+    public static int CalculatePower(int x, int n) {
+        if(n == 0){
+            return 1;
+        }
+        int halfnum = CalculatePower(x, n/2);
+        int halfsq = (halfnum * halfnum);
 
+        if(n % 2 != 0){
+            halfsq = x*halfsq;
+        }
+        return halfsq;
+    }
+    public static void main(String args[]) {
+        int x = 2;
+        int n = 6;
+        int num = CalculatePower(x, n);
+        System.out.println("pwer is " + num);
     }
 }
