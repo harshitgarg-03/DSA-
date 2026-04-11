c82fefffd58ff2f67599b66aec3acc976d24bf67
Harshit Garg <gargh2120@gmail.com>
2026-04-11 11:51:57 +0530

    Friends paring problme using recursion

--- Recursion/FriendsPair.java
+++ Recursion/FriendsPair.java
@@ -1,6 +1,20 @@
 
 public class FriendsPair {
-    public static void main(String args[]) {
+    public static int CalculateWays(int n) {
+        if(n == 1 || n ==2){
+            return n;
+        }
 
+        // Single ways
+        int singleways = CalculateWays(n-1);
+
+        // paired ways 
+        int pairways = CalculateWays(n-2)*(n-1);
+        int totalways = (pairways + singleways);
+
+        return totalways;
+    }
+    public static void main(String args[]) {
+        System.out.println("Total ways are " + CalculateWays(3));
     }
 }
