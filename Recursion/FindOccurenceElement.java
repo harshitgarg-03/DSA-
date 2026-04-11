3f53fcee5786f6f42eaa658fb9f5eba63b608c59
Harshit Garg <gargh2120@gmail.com>
2026-04-11 13:32:11 +0530

    find all occurence of element

--- Recursion/FindOccurenceElement.java
+++ Recursion/FindOccurenceElement.java
@@ -1,5 +1,15 @@
 public class FindOccurenceElement {
+    public static void FindOccur(int arr[],int key, int i) {
+        if(i == arr.length){
+            return;
+        }
+        if(arr[i] == key){
+            System.out.println("occurence of key at index" + i);
+        }
+        FindOccur(arr, key, i+1);
+    }
     public static void main(String args[]) {
-
+        int arr[] = {1, 4, 1, 2, 4, 5, 5, 1, 7, 8, 4, 7};
+        FindOccur(arr, 1, 0);
     }
 }
