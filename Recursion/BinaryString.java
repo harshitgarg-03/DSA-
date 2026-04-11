3087e5f559463398f273c322e2d7eaf3b2ff6585
Harshit Garg <gargh2120@gmail.com>
2026-04-11 12:49:46 +0530

    Print binary string withour consecutive ones

--- Recursion/BinaryString.java
+++ Recursion/BinaryString.java
@@ -1,6 +1,19 @@
 
 public class BinaryString {
-    public static void main(String args[]) {
+    public static void PrintBianry(int term, int lastPlace, String str) {
+        if (term == 0) {
+            System.out.println(str);
+            return;
+        }
 
+        PrintBianry(term - 1, 0, str + "0");
+
+        if (lastPlace == 0) {
+            PrintBianry(term - 1, 1, str + "1");
+        }
+    }
+
+    public static void main(String args[]) {
+        PrintBianry(3, 0, "");
     }
 }
