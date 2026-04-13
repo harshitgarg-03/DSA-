367bf11ba5b9b6cc761c27103a485488394ec7e1
Harshit Garg <gargh2120@gmail.com>
2026-04-13 21:20:23 +0530

    String conversion

--- Recursion/StringConvers.java
+++ Recursion/StringConvers.java
@@ -1,6 +1,32 @@
+import java.util.ArrayList;
 
 public class StringConvers {
-    public static void main(String args[]) {
+    public static void stringConvert(String num, int i, ArrayList<String> arr) {
+        if (i == num.length()) {
+            System.out.println("String is " + arr);
+            return;
+        }
 
+        String numbersInWords[] = {
+                "zero",
+                "one",
+                "two",
+                "three",
+                "four",
+                "five",
+                "six",
+                "seven",
+                "eight",
+                "nine"
+        };
+        
+        arr.add(numbersInWords[num.charAt(i)]);
+        stringConvert(num, i+1, arr);
+    }
+
+    public static void main(String args[]) {
+        // Arra String arr[] = {};
+        ArrayList<String> arr = new ArrayList<>();
+        stringConvert("925", 0, arr);
     }
 }
