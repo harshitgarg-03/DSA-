45a619c7b1f15fa36e428e2cba42d4861c33f6e0
Harshit Garg <gargh2120@gmail.com>
2026-04-14 20:50:08 +0530

    Tower of hanoi prblm using recursion

--- Recursion/TowerofHanoi.java
+++ Recursion/TowerofHanoi.java
@@ -1,6 +1,7 @@
 import java.util.*;
-public class TowerofHanoi {
-public static void main(String args[]) {
 
-}
+public class TowerofHanoi {
+    public static void main(String args[]) {
+
+    }
 }
