dd97f5c51e76c1a441dd02bf2577dfa55a32dc9c
Harshit Garg <gargh2120@gmail.com>
2026-04-14 20:55:51 +0530

    Tower of hanoi prblm using recursion

--- Recursion/TowerofHanoi.java
+++ Recursion/TowerofHanoi.java
@@ -1,16 +1,17 @@
 
 public class TowerofHanoi {
     public static void towerOfHanoi(int n, String src, String helper, String destination) {
-        if(n == 1){
-            System.out.println(n + " disk moves from "+ src + " to " + destination);
+        if (n == 1) {
+            System.out.println(n + " disk moves from " + src + " to " + destination);
             return;
         }
-        
-        towerOfHanoi(n-1, src, destination, helper);
-        System.out.println(n + " disk moves from "+ src + " to " + destination);
-        towerOfHanoi(n-1, helper, src, destination);
+
+        towerOfHanoi(n - 1, src, destination, helper);
+        System.out.println(n + " disk moves from " + src + " to " + destination);
+        towerOfHanoi(n - 1, helper, src, destination);
 
     }
+
     public static void main(String args[]) {
         int n = 1;
         towerOfHanoi(n, "S", "H", "D");
