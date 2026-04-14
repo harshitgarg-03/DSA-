93b9e9d4d5b9c707f5e2335b88e35434a936b77f
Harshit Garg <gargh2120@gmail.com>
2026-04-14 10:21:15 +0530

    Print all substring of astring

--- Recursion/Substring.java
+++ Recursion/Substring.java
@@ -1,6 +1,18 @@
+import java.util.ArrayList;
 
 public class Substring {
-    public static void main(String args[]) {
+    public static void PrintSubstring(String newstr, int i, ArrayList<String> str, StringBuilder sb) {
+        if(i == newstr.length()){
+            System.out.println("substring are " + str);
+            return;
+        }
 
+        sb.append(newstr.charAt(i));
+        System.out.println(sb+ " ");
+        PrintSubstring(newstr, i+1, str, sb);
+    }
+    public static void main(String args[]) {
+        ArrayList<String> str = new ArrayList<>();
+        PrintSubstring("abca", 0, str, new StringBuilder());
     }
 }
