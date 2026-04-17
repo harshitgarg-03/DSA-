f9c6c9e2166167339e3a5d972eaf7fd9d9b23c74
Harshit Garg <gargh2120@gmail.com>
2026-04-17 08:49:53 +0530

    quick sort using recursion

--- Recursion/QuickSort.java
+++ Recursion/QuickSort.java
@@ -11,22 +11,23 @@ public class QuickSort {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
+        return;
     }
 
-    public static void QuikcArr(int arr[], int si, int ei, int i, int j, int pv) {
+    public static void QuikcArr(int arr[], int i, int j, int pv) {
         if (i > arr.length) {
             return;
         }
-        if (arr[i] < pv) {
+        if (arr[i] <= pv) {
             ++j;
-            SwapFun(arr, i, arr.length - 1);
+            SwapFun(arr, i, j);
         }
-        QuikcArr(arr, si, ei, i + 1, j, pv);
+        QuikcArr(arr, i+1, j, pv);
     }
 
     public static void main(String args[]) {
         int arr[] = { 6, 3, 9, 8, 2, 5 };
-        QuikcArr(arr, 0, arr.length - 1, 0, -1, arr[arr.length - 1]);
+        QuikcArr(arr, 0, -1, arr[arr.length-1]);
         PrintArr(arr);
     }
 }
