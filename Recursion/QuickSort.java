3ce56755b58a9d78de23e4880fafbb50c412a8bf
Harshit Garg <gargh2120@gmail.com>
2026-04-18 11:31:50 +0530

    quick sort using recursion

--- Recursion/QuickSort.java
+++ Recursion/QuickSort.java
@@ -11,34 +11,43 @@ public class QuickSort {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
-        return;
     }
 
-    public static int QuikcArr(int arr[], int i, int j, int pv, int pvidx) {
-        if (i == arr.length) {
+    public static int QuikcArr(int arr[], int i, int j, int end, int pv, int pvidx) {
+
+        if (i > end) {
             return pvidx;
         }
+
         if (arr[i] <= pv) {
-            ++j;
+            j++;
             if (arr[i] == pv) {
                 pvidx = j;
             }
             SwapFun(arr, i, j);
         }
-        int newpvidx = QuikcArr(arr, i + 1, j, pv, pvidx);
-        return newpvidx;
+
+        return QuikcArr(arr, i + 1, j, end, pv, pvidx);
     }
-    
+
     public static void mergeArray(int low, int high, int arr[]) {
-        if(low < high){
-            int pivot = arr[high];
-            QuikcArr(arr, pivot, low, high, pivot);
+
+        if (low < high) {
+
+            int pivot = arr[high - 1];
+
+            int pvidx = QuikcArr(arr, low, low - 1, high - 1, pivot, high - 1);
+
+            mergeArray(low, pvidx, arr);
+            mergeArray(pvidx + 1, high, arr);
         }
     }
 
     public static void main(String args[]) {
-        int arr[] = { 6, 3, 9, 8, 2, 5 };
+        int arr[] = {6, 3, 9, 8, 2, 5};
+
         mergeArray(0, arr.length, arr);
+
         PrintArr(arr);
     }
 }
