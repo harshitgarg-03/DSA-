e1bf290c2a693172ce0a5b6ba0a15cdaf5888a7c
Harshit Garg <gargh2120@gmail.com>
2026-04-22 10:41:01 +0530

    merge sort on string array

--- Recursion/MajorityElem.java
+++ Recursion/MajorityElem.java
@@ -6,7 +6,7 @@ public class MajorityElem {
             if (count >= (arr.length - 1 / 2)) {
                 System.out.println("majority elemnet is " + fix);
             } else {
-                System.out.println("majority elemnet not found ");
+                System.out.println("majority elemnet not found "+ count);
             }
             return;
         }
@@ -14,11 +14,11 @@ public class MajorityElem {
         if (arr[i] == fix) {
             count++;
         }
-
+        Major(arr, fix, i+1, count);
     }
 
     public static void main(String args[]) {
-        int arr = { 2, 2, 1, 1, 1, 2, 2, 2 };
+        int arr[] = { 2, 2, 1, 1, 1, 2, 2, 2 };
         Major(arr, arr[0], 1, 1);
     }
 }
