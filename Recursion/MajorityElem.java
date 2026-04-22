95a7f6c240bd50a3ca157cd479219cf175e66995
Harshit Garg <gargh2120@gmail.com>
2026-04-22 10:37:45 +0530

    major element find

--- Recursion/MajorityElem.java
+++ Recursion/MajorityElem.java
@@ -1,7 +1,24 @@
 import java.util.*;
 
 public class MajorityElem {
-    public static void main(String args[]) {
+    public static void Major(int arr[], int fix, int i, int count) {
+        if (i > arr.length - 1) {
+            if (count >= (arr.length - 1 / 2)) {
+                System.out.println("majority elemnet is " + fix);
+            } else {
+                System.out.println("majority elemnet not found ");
+            }
+            return;
+        }
 
+        if (arr[i] == fix) {
+            count++;
+        }
+
+    }
+
+    public static void main(String args[]) {
+        int arr = { 2, 2, 1, 1, 1, 2, 2, 2 };
+        Major(arr, arr[0], 1, 1);
     }
 }
