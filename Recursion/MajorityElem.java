5d8646bfbfb39b047394315140681a52a5c59254
Harshit Garg <gargh2120@gmail.com>
2026-04-22 10:30:11 +0530

    major element find

--- Recursion/MajorityElem.java
+++ Recursion/MajorityElem.java
@@ -1,6 +1,7 @@
 import java.util.*;
-public class MajorityElem {
-public static void main(String args[]) {
 
-}
+public class MajorityElem {
+    public static void main(String args[]) {
+
+    }
 }
