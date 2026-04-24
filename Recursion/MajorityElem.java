598480cab56185615b0aa98900be77cd6568505f
Harshit Garg <gargh2120@gmail.com>
2026-04-24 10:32:26 +0530

    major element find

--- Recursion/MajorityElem.java
+++ Recursion/MajorityElem.java
@@ -5,8 +5,6 @@ public class MajorityElem {
     public static void main(String args[]) {
         int arr[] = { 1, 1, 2, 2, 2, 2, 1, 2, 4, 1, 2, 1, 1};
         Arrays.sort(arr);
-        // System.out.println(arr.length);
-        // System.out.println(arr.length/2);
         System.out.println("major elem is " + arr[(arr.length-1)/2]);
     }
 }         
