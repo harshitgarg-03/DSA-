02e2ba06031ce704513814cf82c56e62ae59bd44
Harshit Garg <gargh2120@gmail.com>
2026-04-25 10:10:49 +0530

    inversion count

--- Recursion/InversionCount.java
+++ Recursion/InversionCount.java
@@ -1,7 +1,30 @@
 import java.util.*;
 
 public class InversionCount {
-    public static void main(String args[]) {
+    public static void FindCount(int arr[], int i, int ei, int count) {
+        if(i > ei-1){
+            System.out.println("count is " + count);
+            return;
+        }
 
+        for(int k = ei-1; k > i; k--){
+            if(arr[i] > arr[k]) {
+                count++;
+            }
+        }
+
+        FindCount(arr, i+1, ei, count);
+
+    }
+    public static void main(String args[]) {
+        int arr[] = {5, 5, 5};
+        int nums[] = arr.clone();
+        Arrays.sort(nums);
+
+        if(Arrays.equals(arr, nums)){
+            System.out.println("Inversion count is 0");
+            return;
+        }
+        FindCount(arr, 0, arr.length, 0);
     }
 }
