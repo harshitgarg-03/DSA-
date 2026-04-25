ba561e156080be3e31b3688da9f101371004d6bb
Harshit Garg <gargh2120@gmail.com>
2026-04-25 09:56:05 +0530

    inversion count

--- Recursion/InversionCount.java
+++ Recursion/InversionCount.java
@@ -1,6 +1,7 @@
 import java.util.*;
-public class InversionCount {
-public static void main(String args[]) {
 
-}
+public class InversionCount {
+    public static void main(String args[]) {
+
+    }
 }
