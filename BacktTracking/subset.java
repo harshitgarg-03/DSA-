98ab0e717ae3b179ed29fdf03d7b827491391b3c
Harshit Garg~ <gargh2120@gmail.com>
2026-05-24 15:47:51 +0530

    subset of string

--- BacktTracking/subset.java
+++ BacktTracking/subset.java
@@ -1,5 +1,20 @@
 public class subset {
-    public static void main(String args[]) {
+    public static void Findsubset(String str, int i, String sb) {
+        if (i == str.length()) {
+            if (sb.length() == 0) {
+                System.out.println(". ");
+                return;
+            }
+            System.out.println(sb + " ");
+            return;
+        }
 
+        Findsubset(str, i + 1, sb + str.charAt(i));
+        Findsubset(str, i + 1, sb);
+    }
+
+    public static void main(String args[]) {
+        String str = "abc";
+        Findsubset(str, 0, "");
     }
 }
