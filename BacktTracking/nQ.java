a3f93091e0031cbb72e6e86a210ed7f57004dc76
Harshit Garg~ <gargh2120@gmail.com>
2026-05-25 12:16:56 +0530

    n quenns

--- BacktTracking/nQ.java
+++ BacktTracking/nQ.java
@@ -1,5 +1,23 @@
 public class nQ {
-    public static void main(String args[]) {
+    public static void printChessBoard(char chessBoard[][]) {
+        for (int i = 0; i < chessBoard.length; i++) {
+            for (int j = 0; j < chessBoard.length; j++) {
+                System.out.println(chessBoard[i][j] + " ");
 
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void main(String args[]) {
+        int n = 4;
+        char chessBoard[][] = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                chessBoard[i][j] = '.';
+            }
+        }
+
+        printChessBoard(chessBoard);
     }
 }
