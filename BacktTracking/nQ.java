89f71e3b6a8358e3b7099dade41c14e8401766c9
Harshit Garg~ <gargh2120@gmail.com>
2026-05-25 22:35:42 +0530

    nquenn

--- BacktTracking/nQ.java
+++ BacktTracking/nQ.java
@@ -9,6 +9,39 @@ public class nQ {
         }
     }
 
+    public static boolean isSafe(char chessBoard[][], int row, int col) {
+
+        // vertical
+        for (int i = 0; i < chessBoard.length; i++) {
+            if (chessBoard[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        // horizontal
+        for (int i = 0; i < chessBoard.length; i++) {
+            if (chessBoard[row][i] == 'Q') {
+                return false;
+            }
+        }
+
+        // Diagonal UP
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (chessBoard[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // Diagonal Down
+        for (int i = row, j = col; i < chessBoard.length && j < chessBoard.length; i++, j++) {
+            if (chessBoard[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public static void placedQueens(char chessBoard[][], int row) {
         if (row == chessBoard.length) {
             return;
