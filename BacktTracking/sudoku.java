dd1dff7ebf0c12186c934e47a0bee971173d805b
Harshit Garg~ <gargh2120@gmail.com>
2026-05-26 18:09:36 +0530

    sudoku solving

--- BacktTracking/sudoku.java
+++ BacktTracking/sudoku.java
@@ -24,13 +24,13 @@ public class sudoku {
                 return false;
         }
 
-        // grid 
-        int sr = (row/3)/3;
-        int sc = (col/3)/3;
+        // grid
+        int sr = (row / 3) * 3;
+        int sc = (col / 3) * 3;
 
-        for(int i = sr; i < sr+3; i++){
-            for(int j = sc; j < sc+3; j++){
-                if(sudoku[i][j] == digit){
+        for (int i = sr; i < sr + 3; i++) {
+            for (int j = sc; j < sc + 3; j++) {
+                if (sudoku[i][j] == digit) {
                     return false;
                 }
             }
@@ -38,23 +38,44 @@ public class sudoku {
         return true;
     }
 
-    public static void SudokuSolver(int sudoku[][], int row) {
-        if (row == sudoku.length) {
-            printsudoku(sudoku);
-            return;
-        }
+    public static boolean SudokuSolver(int sudoku[][], int row, int col) {
 
-        for (int i = 0; i < sudoku.length; i++) {
-            for (int j = 1; j <= 9; j++) {
-                if (sudoku[row][i] == 0 && isSafe(sudoku, row, i, j)) {
-                    sudoku[row][i] = j;
-                }
-            }
-        }
-        SudokuSolver(sudoku, row + 1);
+    if (row == sudoku.length) {
+        printsudoku(sudoku);
+        return true;
     }
 
-    public static void main(String args[]) {
+    int nextrow = row;
+    int nextcol = col + 1;
+
+    if (col + 1 == sudoku.length) {
+        nextrow = row + 1;
+        nextcol = 0;
+    }
+
+    if (sudoku[row][col] != 0) {
+        return SudokuSolver(sudoku, nextrow, nextcol);
+    }
+
+    for (int j = 1; j <= 9; j++) {
+
+        if (isSafe(sudoku, row, col, j)) {
+
+            sudoku[row][col] = j;
+
+            // recurse
+            if (SudokuSolver(sudoku, nextrow, nextcol)) {
+                return true;
+            }
+
+            // backtracking
+            sudoku[row][col] = 0;
+        }
+    }
+
+    return false;
+}
+        public static void main(String args[]) {
         int sudoku[][] = {
                 { 3, 0, 6, 5, 0, 8, 4, 0, 0 },
                 { 5, 2, 0, 0, 0, 0, 0, 0, 0 },
@@ -66,6 +87,6 @@ public class sudoku {
                 { 0, 0, 0, 0, 0, 0, 0, 7, 4 },
                 { 0, 0, 5, 2, 0, 6, 3, 0, 0 }
         };
-        SudokuSolver(sudoku, 0);
+        SudokuSolver(sudoku, 0, 0);
     }
 }
