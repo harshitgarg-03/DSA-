802f59c8065f20665d1ed1a7adbec4fa28a394e4
Harshit Garg~ <gargh2120@gmail.com>
2026-05-30 13:36:46 +0530

    permutaion of string

--- BacktTracking/permutation.java
+++ BacktTracking/permutation.java
@@ -1,6 +1,19 @@
 public class permutation {
-    public static void main(String args[]) {
+    public static void findPermutation(String str, String ans) {
+        if(str.length()==0){
+            System.out.println(ans + "");
+            return;
+        }
 
+        for(int i = 0; i < str.length(); i++){
+            Char curr = str.charAt(i);
+            String NewStr = str.substring(0, i) + str.substring(i+1);
+            findPermutation(NewStr, ans+curr);
+        }
+    }
+    public static void main(String args[]) {
+        String str = "123";
+        findPermutation(str, "");
     }
 }
 
