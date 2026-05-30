5fae37ad364ff1d4f39b4c195ea78c66a0332b25
Harshit Garg~ <gargh2120@gmail.com>
2026-05-30 13:47:56 +0530

    find all arrangements of string

--- BacktTracking/permutation.java
+++ BacktTracking/permutation.java
@@ -6,13 +6,13 @@ public class permutation {
         }
 
         for(int i = 0; i < str.length(); i++){
-            Char curr = str.charAt(i);
+            char curr = str.charAt(i);
             String NewStr = str.substring(0, i) + str.substring(i+1);
             findPermutation(NewStr, ans+curr);
         }
     }
     public static void main(String args[]) {
-        String str = "123";
+        String str = "abc";
         findPermutation(str, "");
     }
 }
