d320549dcbe6f7530a7656fffb7245ce3e6cd550
Harshit Garg~ <gargh2120@gmail.com>
2026-06-07 09:09:13 +0530

    max contain water

--- ArrayList/ContainWater.java
+++ ArrayList/ContainWater.java
@@ -1,5 +1,23 @@
-package ArrayList;
 
 public class ContainWater {
-    
+
+    public static void main(String[] args) {
+
+        int height[] = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+
+        int maxWater = 0;
+        int n = height.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int min = Math.min(height[i], height[j]);
+                int vol = min * (j-i);
+                if (vol > maxWater) {
+                    maxWater = vol;
+                }
+            }
+        }
+
+        System.out.println("max volume is " + maxWater);
+    }
 }
