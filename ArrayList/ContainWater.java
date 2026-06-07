935d967649dad0ef64c85434c35d1c4992dde700
Harshit Garg~ <gargh2120@gmail.com>
2026-06-07 09:27:23 +0530

    max contain water

--- ArrayList/ContainWater.java
+++ ArrayList/ContainWater.java
@@ -8,16 +8,33 @@ public class ContainWater {
         int maxWater = 0;
         int n = height.length;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int min = Math.min(height[i], height[j]);
-                int vol = min * (j-i);
-                if (vol > maxWater) {
-                    maxWater = vol;
-                }
+        // O(n^2) approach
+        // for (int i = 0; i < n; i++) {
+        // for (int j = i + 1; j < n; j++) {
+        // int min = Math.min(height[i], height[j]);
+        // int vol = min * (j-i);
+        // if (vol > maxWater) {
+        // maxWater = vol;
+        // }
+        // }
+        // }
+
+        // O(n) approach
+        int lp = 0;
+        int rp = n - 1;
+        while (lp < rp) {
+            int min = Math.min(height[lp], height[rp]);
+            int vol = min * (rp - lp);
+            if (vol > maxWater) {
+                maxWater = vol;
+            }
+
+            if(lp < rp){
+                lp++;
+            } else {
+                rp--;
             }
         }
-
         System.out.println("max volume is " + maxWater);
     }
 }
