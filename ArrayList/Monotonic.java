523b81bb1397d1546a1b21299f79eee34ba1eed9
Harshit Garg~ <gargh2120@gmail.com>
2026-06-08 15:23:37 +0530

    monotonic arraylist

--- ArrayList/Monotonic.java
+++ ArrayList/Monotonic.java
@@ -1,3 +1,32 @@
 public class Monotonic {
-    
+    public static void main(String[] args) {
+        int nums[] = {1,3,2};
+
+        int n = nums.length;
+        int asc = 0;
+        int desc = 0;
+        int lp = 0;
+        int ptr = lp+1;
+
+        while(ptr < n){
+            if(nums[lp] == nums[ptr]){
+                asc++;
+                desc++;
+            }
+            else if(nums[lp] < nums[ptr]){
+                asc++;
+            }
+            else{
+                desc++;
+            }
+            lp++;
+            ptr++;
+        }
+
+        if(asc == n-1 || desc == n-1){
+            System.out.println("It's Monotonic ");
+        } else {
+            System.out.println("It's Not Monotonic");
+        }
+    }
 }
