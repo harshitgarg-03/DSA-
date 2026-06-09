aa6a4d30ad27da7463128c97ae94a2627a09df91
Harshit Garg~ <gargh2120@gmail.com>
2026-06-09 08:50:54 +0530

    count inversion

---
+++ Array/1D-Array/CountIvr.java
@@ -0,0 +1,25 @@
+public class CountIvr {
+    public static void main(String[] args) {
+        int nums[] = {5, 3, 2, 4, 1};
+
+        int n = nums.length;
+        int lptr = 0;
+        int rptr = n-1;
+        int count = 0;
+        while(lptr < n){
+            if(rptr <= lptr){
+                rptr = n-1;
+                lptr++;
+            }
+
+            else if((lptr < rptr) && (nums[lptr] > nums[rptr])){
+                System.out.println(nums[lptr] + "," + nums[rptr]);
+                count++;
+            }
+
+            rptr--;
+        }
+
+        System.out.println("count is " + count);
+    }
+}
