9758abd58a359e318f21448bfe5f5169ca558bee
Harshit Garg~ <gargh2120@gmail.com>
2026-06-11 11:33:30 +0530

    Combination of arary

--- Practice-Sheet/Basic/Combination.java
+++ Practice-Sheet/Basic/Combination.java
@@ -4,36 +4,29 @@ import java.util.Arrays;
 
 public class Combination {
 
-    public static void solve(int arr[], int target, int i, ArrayList<Integer> list) {
-        if(i == arr.length){
+    public static void solve(int arr[], int target, int i, ArrayList<Integer> list, int sum) {
+        if (i == arr.length || sum > target) {
+            return;
+        }
+
+        if (sum == target) {
+            System.out.println(list);
             return;
         }
 
         list.add(arr[i]);
-    
-        int sum = list.stream().mapToInt(Integer::intValue).sum();
-        ArrayList<Integer> Dlist = new ArrayList<>();
+        solve(arr, target, i, list, sum + arr[i]);
+        list.remove(list.size() - 1);
 
-        if(arr[i] == target) {
-            System.out.println(arr[i] + "");
-        }
-        while(sum < target){
-
-            for (int index = i+1; index < arr.length; index++) {
-                if(sum + arr[index] == target){
-                    System.out.println(list + ", " + arr[index]);
-                }
-            }
-        }
-
-
+        solve(arr, target, i + 1, list, sum);
     }
+
     public static void main(String[] args) {
-        int arr[] = {2, 3, 6, 7};
+        int arr[] = { 2, 3, 6, 7 };
         int target = 7;
         ArrayList<Integer> list = new ArrayList<>();
 
         Arrays.sort(arr);
-        solve(arr, target, 0, list);
+        solve(arr, target, 0, list, 0);
     }
 }
