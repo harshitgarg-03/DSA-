9e3ce919fd84db9ca9888263d471b3426e276cbf
Harshit Garg~ <gargh2120@gmail.com>
2026-06-16 21:54:37 +0530

    all sequences

---
+++ Practice-Sheet/Basic/PallindromePartioning.java
@@ -0,0 +1,19 @@
+public class PallindromePartioning {
+
+    public static void solve(String str, int idx, String ans) {
+        if(idx == str.length()) {
+            System.out.println(ans);
+            return;
+        }
+
+        solve(str, idx + 1,
+              ans + str.charAt(idx));
+
+        solve(str, idx + 1,
+              ans);
+
+    }
+    public static void main(String args[]) {
+    solve("aab", 0, "");
+    }
+}
