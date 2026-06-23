800b4e7bdc613fa599d31574e6ce7718f3696e26
Harshit Garg~ <gargh2120@gmail.com>
2026-06-23 11:05:30 +0530

    insert an element at bottom of the stcak

--- Stack/pushBottom.java
+++ Stack/pushBottom.java
@@ -1,5 +1,27 @@
+
+import java.util.Stack;
+
 public class pushBottom {
-    public static void main(String[] args) {
+
+    public static void PushhingLow(Stack<Integer> s, int data, int elem){
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
         
+        elem = s.pop();
+        PushhingLow(s, data, elem);
+        s.push(elem);
+    }
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(3);
+        s.push(2);
+        s.push(1);
+        // System.out.println(s);
+        PushhingLow(s, 4, -1);
+        PushhingLow(s, 5, -1);
+        PushhingLow(s, 6, -1);
+        System.out.println(s);
     }
 }
