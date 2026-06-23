8976bf6d84a0250ce2c2d9a2e2a0b9362fd508de
Harshit Garg~ <gargh2120@gmail.com>
2026-06-23 11:12:33 +0530

    reverse string

---
+++ Stack/reverseString.java
@@ -0,0 +1,12 @@
+
+import java.util.Stack;
+
+public class reverseString {
+    public static void main(String[] args) {
+        Stack <Character> s = new Stack<>();
+        s.push('a');
+        s.push('b');
+        s.push('c');
+
+    }
+}
