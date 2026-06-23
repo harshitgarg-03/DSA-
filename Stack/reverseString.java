fefc204f3a896ae48e610dfe86b805b3366456c4
Harshit Garg~ <gargh2120@gmail.com>
2026-06-23 13:02:54 +0530

    reverse string

--- Stack/reverseString.java
+++ Stack/reverseString.java
@@ -2,11 +2,25 @@
 import java.util.Stack;
 
 public class reverseString {
+
+    public static void changeString(Stack <Character> s, String str){
+        if(s.isEmpty()){
+            System.out.println("reverse string is  " + str);
+            return;
+        }
+
+        char ch = s.pop();
+
+        str += ch;
+        changeString(s, str);
+    }
     public static void main(String[] args) {
         Stack <Character> s = new Stack<>();
         s.push('a');
         s.push('b');
         s.push('c');
 
+        System.out.println("string is "+ s);
+        changeString(s, "");
     }
 }
