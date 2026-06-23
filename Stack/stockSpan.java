baa70a8dcfa78f05a97e10aac4ae2811af9ab460
Harshit Garg~ <gargh2120@gmail.com>
2026-06-23 15:03:21 +0530

    stock span

--- Stack/stockSpan.java
+++ Stack/stockSpan.java
@@ -1,3 +1,16 @@
+import java.util.Stack;
+
 public class stockSpan {
-    
+    public static void main(String[] args) {
+        int price[] = {100, 80, 60, 70, 60, 85, 100};
+
+        int span[] = new int[price.length];
+
+        Stack<Integer> s = new Stack<>();
+        span[0] = 1;
+        s.push(0);
+        for (int i = 1; i < price.length; i++){
+
+        }
+    }
 }
