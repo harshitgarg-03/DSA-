93694e25b9e735a9b0a46279edfdc05fba8f7bed
Harshit Garg~ <gargh2120@gmail.com>
2026-06-23 09:10:58 +0530

    stack implemented using arraylist

---
+++ Stack/stack.java
@@ -0,0 +1,51 @@
+
+import java.util.ArrayList;
+
+public class stack {
+
+    public static class stackUsingArrlist {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        public boolean isEmpty() {
+            return list.size() == 0;
+        }
+
+        public void push(int data) {
+            list.add(data);
+        }
+
+        public int pop() {
+            if (isEmpty()) {
+                return -1;
+            }
+
+            int data = list.get(list.size() - 1);
+            list.remove(list.size() - 1);
+
+            return data;
+        }
+
+        public int peek() {
+            if (isEmpty()) {
+                return -1;
+            }
+
+            return list.get(list.size() - 1);
+        }
+
+    }
+
+    public static void main(String args[]) {
+        stackUsingArrlist stack = new stackUsingArrlist();
+
+        stack.push(3);
+        stack.push(2);
+        stack.push(1);
+
+        while (!stack.isEmpty()) { 
+            System.out.println(stack.pop());
+        }
+
+        System.out.println(stack.isEmpty());
+    }
+}
