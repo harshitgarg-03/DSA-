31ace3da1038d35cb4877cb1a155b7bcc800265d
Harshit Garg~ <gargh2120@gmail.com>
2026-06-23 15:21:09 +0530

    stock span

--- Stack/stockSpan.java
+++ Stack/stockSpan.java
@@ -10,7 +10,25 @@ public class stockSpan {
         span[0] = 1;
         s.push(0);
         for (int i = 1; i < price.length; i++){
+            int stockPrice = price[i];
 
+            while(!s.isEmpty()){
+                int prevStock = price[s.peek()];
+                if(prevStock > stockPrice){
+                    span[i] = i-s.peek();
+                    s.push(i);
+                    break;
+                }
+                s.pop();
+            }
+
+            if(span[i] == 0){
+                span[i] = i+1;
+            }
+        }
+
+        for (int i = 0; i < span.length; i++) {
+            System.out.print( span[i] + " ");
         }
     }
 }
