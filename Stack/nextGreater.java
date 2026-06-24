5bae0ca12826cb5ca5c18860e60bab45c26b68a7
Harshit Garg~ <gargh2120@gmail.com>
2026-06-24 13:45:45 +0530

    nextGreater

--- Stack/nextGreater.java
+++ Stack/nextGreater.java
@@ -1,5 +1,34 @@
+
+import java.util.Stack;
+
 public class nextGreater {
     public static void main(String[] args) {
+        int arr[] = {2, 1, 7, 11, 12, 3, 6, 9};
+        int nextG[] = new int[arr.length];
         
+        Stack<Integer> s = new Stack<>();
+
+        nextG[arr.length-1] = -1;
+        s.push(arr[arr.length-1]);
+
+        for(int i = arr.length-2; i >= 0; i--){
+            while(!s.isEmpty()){
+                if(s.peek()> arr[i]){
+                    nextG[i] = s.peek();
+                    s.push(arr[i]);
+                    break;
+                }
+            }
+
+            if(nextG[i] == 0){
+                nextG[i] = -1;
+                s.push(arr[i]);
+            }
+        }
+
+        for (int idx = 0; idx < nextG.length; idx++) {
+            System.out.println(nextG[idx]);
+            
+        }
     }
 }
