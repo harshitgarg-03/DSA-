caf9abf39fcdb4093ee5e43c2265eeac1f872e37
Harshit Garg~ <gargh2120@gmail.com>
2026-06-24 16:05:47 +0530

    duplicate parenthesis

--- Stack/DuplicateParan.java
+++ Stack/DuplicateParan.java
@@ -1,3 +1,28 @@
+import java.util.Stack;
+
 public class DuplicateParan {
-    
+    public static boolean main(String[] args) {
+        String str = "(((a+(b)))+(c+d))";
+        Stack<Character> s = new Stack<>();
+        int active = 0;
+
+        for (int idx = 0; idx < str.length(); idx++) {
+            Character ch = str.charAt(idx);
+            if(ch != ')'){
+                s.push(ch);
+            }else{
+                while (s.peek() != '(') { 
+                    active = 1;
+                    s.pop();
+                }
+
+                if(!(s.peek() == ')' && active == 1)){
+                    return true;
+                }
+                active = 0;
+            }
+        }
+
+        return false;
+    }
 }
