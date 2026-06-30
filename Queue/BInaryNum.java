b5f57ceffd26626c0dd73b35055f36c0d08ec55e
Harshit Garg~ <gargh2120@gmail.com>
2026-06-30 10:30:14 +0530

    binary numbers

--- Queue/BInaryNum.java
+++ Queue/BInaryNum.java
@@ -1,3 +1,25 @@
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BInaryNum {
-    
+    public static void main(String[] args) {
+        int n = 5;
+        ArrayList<String> list = new ArrayList<>();
+        Queue<String> q = new LinkedList<>();
+
+        q.add("1");
+
+        while(n-- > 0){
+            String temp = q.remove();
+            list.add(temp);
+
+            q.add(temp + "0");
+            q.add(temp + "1");
+            
+        }
+
+        System.out.println(list);
+    }
 }
