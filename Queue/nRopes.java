a30e2eb5cdfeccc32eaf507dd5b7c3df4bc54502
Harshit Garg~ <gargh2120@gmail.com>
2026-07-02 08:23:33 +0530

    connected n ropes

---
+++ Queue/nRopes.java
@@ -0,0 +1,27 @@
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class nRopes {
+    public static void main(String args[]) {
+        int arr[] = {4, 3, 2, 6};
+        Arrays.sort(arr);
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i = 0; i < arr.length; i++){
+            q.add(arr[i]);
+        }
+        int sum = 0;
+        int data = q.remove();
+
+        while(!q.isEmpty()){
+            int qdata = q.remove();
+            data = (qdata+data);
+            sum = (sum + data);
+        }
+
+        System.out.println("minimum cost is " + sum);
+    }
+}
