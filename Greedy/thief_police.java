459c2f432cc1d691f8d187ebd093141d15801578
Harshit Garg~ <gargh2120@gmail.com>
2026-07-04 11:47:35 +0530

    thief and police

--- Greedy/thief_police.java
+++ Greedy/thief_police.java
@@ -1,7 +1,8 @@
 public class thief_police {
     public static void main(String[] args) {
-        char arr[] = {'P', 'T', 'T', 'P', 'T', 'T', 'T', 'P', 'T'};
+        char arr[] = { 'P', 'T', 'T', 'P', 'T', 'T', 'T', 'P', 'T' };
 
+        int range = 1;
         int count = 0;
 
         int j = 0;
@@ -9,21 +10,78 @@ public class thief_police {
         int Pidx = -1;
         int Tidx = -1;
 
-        // Find First Police and Thief 
-        for(int i = 0; i < arr.length; i++){
-            if(j == 0 && arr[i] == 'P'){
+        // Find First Police and Thief
+        for (int i = 0; i < arr.length; i++) {
+            if (j == 0 && arr[i] == 'P') {
                 j = -1;
                 Pidx = i;
-            } 
-            else if (k == 0 && arr[i] == 'T'){
+            } else if (k == 0 && arr[i] == 'T') {
                 k = -1;
                 Tidx = i;
-            }
-            else if (k == -1 && j == -1){
+            } else if (k == -1 && j == -1) {
                 break;
             }
         }
 
         System.out.println(Pidx + " " + Tidx);
+
+        while (Pidx != -1 && Tidx != -1) {
+
+            int distance = Math.abs(Pidx - Tidx);
+
+            if (distance <= range) {
+
+                count++;
+
+                int oldP = Pidx;
+                int oldT = Tidx;
+
+                Pidx = -1;
+                Tidx = -1;
+
+                for (int i = oldP + 1; i < arr.length; i++) {
+                    if (arr[i] == 'P') {
+                        Pidx = i;
+                        break;
+                    }
+                }
+
+                for (int i = oldT + 1; i < arr.length; i++) {
+                    if (arr[i] == 'T') {
+                        Tidx = i;
+                        break;
+                    }
+                }
+
+            } else {
+
+                if (Pidx < Tidx) {
+
+                    int oldP = Pidx;
+                    Pidx = -1;
+
+                    for (int i = oldP + 1; i < arr.length; i++) {
+                        if (arr[i] == 'P') {
+                            Pidx = i;
+                            break;
+                        }
+                    }
+
+                } else {
+
+                    int oldT = Tidx;
+                    Tidx = -1;
+
+                    for (int i = oldT + 1; i < arr.length; i++) {
+                        if (arr[i] == 'T') {
+                            Tidx = i;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println("count is :: " + count);
     }
 }
