16a707179cee5ff2babfd5e100fdce74616262b7
Harshit Garg~ <gargh2120@gmail.com>
2026-07-05 12:13:59 +0530

    maximum bag return with full capacity

--- Greedy/Max_Bags.java
+++ Greedy/Max_Bags.java
@@ -1,10 +1,21 @@
+
+import java.util.Arrays;
+
 public class Max_Bags {
     public static void main(String[] args) {
-        int CP[] = {2, 3, 4, 5};
-        int RK[] = {1, 2, 4, 4};
+        Integer CP[] = {2, 3, 4, 5};
+        Integer RK[] = {1, 2, 4, 4};
 
         int AB = 2;
 
-        
+        Integer IdxArr[] = new Integer[CP.length];
+
+        for(int i = 0; i < IdxArr.length; i++){
+            IdxArr[i] = i;
+        }
+
+        Arrays.sort(IdxArr, (a,b) -> (CP[a]-RK[a])-(CP[b]-RK[b]));
+
+        System.out.println(Arrays.toString(IdxArr));
     }
 }
