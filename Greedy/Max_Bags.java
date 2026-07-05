f780be94d9f9e0f1c9e0d7decf1177800e390827
Harshit Garg~ <gargh2120@gmail.com>
2026-07-05 12:10:22 +0530

    maximum bag return with full capacity

---
+++ Greedy/Max_Bags.java
@@ -0,0 +1,10 @@
+public class Max_Bags {
+    public static void main(String[] args) {
+        int CP[] = {2, 3, 4, 5};
+        int RK[] = {1, 2, 4, 4};
+
+        int AB = 2;
+
+        
+    }
+}
