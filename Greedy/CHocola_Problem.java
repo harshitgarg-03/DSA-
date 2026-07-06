4b27a3d039b9c1596b03686b2ea11100fd3dc33a
Harshit Garg~ <gargh2120@gmail.com>
2026-07-06 11:43:27 +0530

    chocola problem

--- Greedy/CHocola_Problem.java
+++ Greedy/CHocola_Problem.java
@@ -2,19 +2,18 @@
 import java.util.Arrays;
 import java.util.Collections;
 
-
 public class CHocola_Problem {
     public static void main(String args[]) {
         int m = 3;
         int n = 2;
 
-        Integer hc[] = {1, 3}; // horizon cuts 
-        Integer vc[] = {5}; // vertical cuts 
+        Integer hc[] = { 7}; // horizon cuts
+        Integer vc[] = { 4 }; // vertical cuts
 
         int cost = 0;
-        int pieces = 0; // total piece 
-        int vp = 1; // vertical piece 
-        int hp = 1; // horizontal piece 
+        int pieces = 0; // total piece
+        int vp = 1; // vertical piece
+        int hp = 1; // horizontal piece
 
         Arrays.sort(hc, Collections.reverseOrder());
         Arrays.sort(vc, Collections.reverseOrder());
@@ -22,19 +21,35 @@ public class CHocola_Problem {
         int i = 0;
         int j = 0;
 
-        while(pieces < m*n && (i < hc.length || j < vc.length)){
-            if(hc[i] >= vc[j]){
+        while (i < hc.length && j < vc.length) {
+            if (hc[i] >= vc[j]) {
                 hp++;
-                cost += (hc[i]*vp);
+                cost += (hc[i] * vp);
                 i++;
-            }else {
+            } else {
+
                 vp++;
-                cost += (vc[i]*hp);
+                cost += (vc[j] * hp);
                 j++;
+
             }
-            pieces = hp*vp;
+            pieces = hp * vp;
         }
 
-        System.out.println("cost is :: "+ cost);
+        while (i < hc.length) {
+            hp++;
+            cost += (hc[i] * vp);
+            i++;
+            pieces = hp * vp;
+        }
+
+        while (j < vc.length) {
+            vp++;
+            cost += (vc[j] * hp);
+            j++;
+            pieces = hp * vp;
+        }
+
+        System.out.println("cost is :: " + cost);
     }
 }
