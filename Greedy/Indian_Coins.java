fa505887ef5084a5cabc764b692d84f7ee127e67
Harshit Garg~ <gargh2120@gmail.com>
2026-07-07 09:53:42 +0530

    indian coins

--- Greedy/Indian_Coins.java
+++ Greedy/Indian_Coins.java
@@ -1,9 +1,12 @@
+
+import java.util.Arrays;
+
 public class Indian_Coins {
     public static void main(String args[]) {
-        int dem[] = { 1, 2, 5, 10, 20, 50, 100, 500 };
+        int dem[] = {1, 2, 5, 10, 20, 50, 100, 200, 500};
         int value = 2012;
         int count = 0;
-
+        Arrays.sort(dem);
         for (int i = dem.length - 1; i >= 0; i--) {
             if (value >= dem[i] && value > 0) {
                 System.out.println("notes is " + dem[i]);
