3c4059729a3834b70be3eac173ef966d8b626c9b
Harshit Garg~ <gargh2120@gmail.com>
2026-07-10 09:18:30 +0530

    assign cookies

--- Greedy/Assign_Cookie.java
+++ Greedy/Assign_Cookie.java
@@ -1,5 +1,29 @@
+
+import java.util.Arrays;
+
 public class Assign_Cookie {
     public static void main(String args[]) {
+        int g[] = {1, 2, 3};
+        int s[] = {1, 1};
 
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int count = 0;
+        int i = 0;
+        int j = 0;
+
+        while(i < g.length && j < s.length){
+            if(s[j] >= g[i]){
+                count++;
+                i++;
+                j++;
+            }
+            else if (g[i] > s[j]) {
+                j++;
+            }
+        }
+
+        System.out.println("Max cookie assign is :: "+ count);
     }
 }
