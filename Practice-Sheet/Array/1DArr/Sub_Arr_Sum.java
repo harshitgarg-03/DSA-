d1f334cf0a7f615e3610c5053516770f734e9b9f
Harshit Garg~ <gargh2120@gmail.com>
2026-07-11 13:38:26 +0530

    subarray sum

--- Practice-Sheet/Array/1DArr/Sub_Arr_Sum.java
+++ Practice-Sheet/Array/1DArr/Sub_Arr_Sum.java
@@ -1,3 +1,31 @@
+// later do it hashing concept using 
+import java.util.Arrays;
+
 public class Sub_Arr_Sum {
-    
+    public static void main(String[] args) {
+        int arr[] = {1,2, 3};
+        int pref[] = new int[arr.length];
+        int k = 3;
+        int count = 0;
+
+        int sum = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            sum += arr[i];
+            pref[i] = sum;
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            int elem = arr[i];
+            int find_elem = k-elem;
+
+            if(find_elem == k){
+                count++;
+            }else {
+                
+            }
+        }
+
+        System.out.println("count is :: " + Arrays.toString(pref));
+    }
 }
