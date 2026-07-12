da267fa29d8b32f8e619a513cb56fc704035f1a5
Harshit Garg~ <gargh2120@gmail.com>
2026-07-12 13:18:04 +0530

    spiral matrix

--- Practice-Sheet/Array/2DArr/Spiral_Matrix.java
+++ Practice-Sheet/Array/2DArr/Spiral_Matrix.java
@@ -1,33 +1,40 @@
+
+
 public class Spiral_Matrix {
     public static void main(String[] args) {
-        int matr[][] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {11, 12, 13}};
+        int matr[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }};
 
-        int m = matr.length-1;
-        int n = matr[0].length-1;
+        int m = matr.length - 1;
+        int n = matr[0].length - 1;
 
         int row = 0;
         int col = 0;
+        while (row <= m && col <= n) {
 
-        // right
-        for(int i = col; i <= n; i++){
-            System.out.print(matr[row][i] + " ");
+            // right
+            for (int i = col; i <= n; i++) {
+                System.out.print(matr[row][i] + " ");
+            }
+            row++;
+            // down
+            for (int i = row; i <= m; i++) {
+                System.out.print(matr[i][n] + " ");
+            }
+            n--;
+            // left
+            if(row <= m){
+                for (int i = n; i >= col; i--) {
+                    System.out.print(matr[m][i] + " ");
+                }
+                m--;
+            }
+            // up
+            if(col <= n){
+                for (int i = m; i >= row; i--) {
+                    System.out.print(matr[i][col] + " ");
+                }
+                col++;
+            }
         }
-        row++;
-        // down
-        for(int i = row; i <= m; i++){
-            System.out.print(matr[i][n] + " ");
-        }
-        n--;
-        // left
-        for(int i = n; i >= col; i--){
-            System.out.print(matr[m][i] + " ");
-        }
-        m--;
-        // up
-        for(int i = m; i >= row; i--){
-            System.out.print(matr[i][col] + " ");
-        }
-        col++;
-
     }
 }
