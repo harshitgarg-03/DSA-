ae1c7fddb99ecbc2f1e669b67375b5bc26a1c400
Harshit Garg~ <gargh2120@gmail.com>
2026-07-12 12:34:12 +0530

    spiral matrix

--- Practice-Sheet/Array/2DArr/Spiral_Matrix.java
+++ Practice-Sheet/Array/2DArr/Spiral_Matrix.java
@@ -1,3 +1,33 @@
 public class Spiral_Matrix {
-    
+    public static void main(String[] args) {
+        int matr[][] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {11, 12, 13}};
+
+        int m = matr.length-1;
+        int n = matr[0].length-1;
+
+        int row = 0;
+        int col = 0;
+
+        // right
+        for(int i = col; i <= n; i++){
+            System.out.print(matr[row][i] + " ");
+        }
+        row++;
+        // down
+        for(int i = row; i <= m; i++){
+            System.out.print(matr[i][n] + " ");
+        }
+        n--;
+        // left
+        for(int i = n; i >= col; i--){
+            System.out.print(matr[m][i] + " ");
+        }
+        m--;
+        // up
+        for(int i = m; i >= row; i--){
+            System.out.print(matr[i][col] + " ");
+        }
+        col++;
+
+    }
 }
