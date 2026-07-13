7d73eb2ca280be4e73c7cf6601974903fa861e9d
Harshit Garg~ <gargh2120@gmail.com>
2026-07-13 07:55:04 +0530

    rotate_image

--- Practice-Sheet/Array/2DArr/Rotate_Img.java
+++ Practice-Sheet/Array/2DArr/Rotate_Img.java
@@ -1,6 +1,20 @@
-import java.util.Arrays;
 
 public class Rotate_Img {
+
+    static void reverseRow(int[] row) {
+        int left = 0;
+        int right = row.length - 1;
+
+        while (left < right) {
+            int temp = row[left];
+            row[left] = row[right];
+            row[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
     public static void main(String[] args) {
         int arr[][] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         int n = arr.length;
@@ -11,9 +25,11 @@ public class Rotate_Img {
                 arr[j][i] = elem;
             }
         }
-        for (int i = 0; i < arr.length; i++) {
-            Arrays.sort(arr[i]);
+        
+        for(int[] row: arr){
+            reverseRow(row);
         }
+
         for(int i = 0; i < arr.length; i++){
             for(int j = 0; j < arr.length; j++){
                 System.out.print(arr[i][j] + " ");
