7fa42b35bfb98a666aeb9705ad2829292675d0dd
Harshit Garg~ <gargh2120@gmail.com>
2026-07-13 12:45:40 +0530

    all permutation

---
+++ Practice-Sheet/Array/1DArr/All_Permutation.java
@@ -0,0 +1,16 @@
+public class All_Permutation {
+
+    public static void All_Pair(int arr[], String str) {
+        if(str.length() >= arr.length){
+            return;
+        }
+
+
+    }
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3};
+        
+        String str = "";
+        All_Pair(arr, str);
+    }
+}
