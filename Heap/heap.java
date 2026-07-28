b3eda4f73f3488036ea90dcb088e7efe8ae1aafd
Harshit Garg~ <gargh2120@gmail.com>
2026-07-28 22:11:59 +0530

    heap add operation defined

---
+++ Heap/heap.java
@@ -0,0 +1,15 @@
+
+import java.util.ArrayList;
+
+public class heap {
+
+    static class Heap {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        
+    }
+
+    public static void main(String args[]) {
+
+    }
+}
