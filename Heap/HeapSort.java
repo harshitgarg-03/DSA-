718369627d08f71c53ea710c7300087647d83e42
Harshit Garg~ <gargh2120@gmail.com>
2026-07-30 12:03:17 +0530

    heap sort

--- Heap/HeapSort.java
+++ Heap/HeapSort.java
@@ -1,20 +1,19 @@
 public class HeapSort {
 
     public static void heapify(int arr[], int i, int size) {
-        int leftIdx = 2*i+1;
-        int rigthIdx = 2*i+2;
+        int leftIdx = 2 * i + 1;
+        int rigthIdx = 2 * i + 2;
 
         int maxIdx = i;
 
-
-        if(leftIdx < size && arr[leftIdx] > arr[maxIdx] ){
+        if (leftIdx < size && arr[leftIdx] > arr[maxIdx]) {
             maxIdx = leftIdx;
         }
-        if(rigthIdx < size && arr[rigthIdx] > arr[maxIdx]){
+        if (rigthIdx < size && arr[rigthIdx] > arr[maxIdx]) {
             maxIdx = rigthIdx;
         }
 
-        if(maxIdx != i){
+        if (maxIdx != i) {
             int temp = arr[i];
             arr[i] = arr[maxIdx];
             arr[maxIdx] = temp;
@@ -26,12 +25,11 @@ public class HeapSort {
     public static void HeapSorting(int arr[]) {
         int n = arr.length;
 
-
-        for(int i = n/2; i >= 0; i--){ // max HEap
+        for (int i = n / 2; i >= 0; i--) { // max HEap
             heapify(arr, i, n);
         }
 
-        for(int i = n-1; i >= 0; i--){
+        for (int i = n - 1; i >= 0; i--) {
             int temp = arr[0];
             arr[0] = arr[i];
             arr[i] = temp;
@@ -39,14 +37,14 @@ public class HeapSort {
             heapify(arr, 0, i);
         }
 
-
     }
+
     public static void main(String[] args) {
         // int arr[] = {1, 2, 4, 5, 3};
-        int arr[] = {4, 1, 3, 8, 7, 10, 12};
+        int arr[] = { 4, 1, 3, 9, 7 };
 
         HeapSorting(arr);
-        for(int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
