f32d73c0ded7d4549736123c54db268205de32c1
Harshit Garg~ <gargh2120@gmail.com>
2026-08-03 11:07:43 +0530

    sort fqs

---
+++ Hashing/Sort_fq.java
@@ -0,0 +1,19 @@
+
+import java.util.HashMap;
+
+public class Sort_fq {
+    public static void main(String[] args) {
+        String str = "cccaaa";
+        HashMap<Character, Integer> hm = new HashMap<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (hm.containsKey(ch)) {
+                hm.put(ch, hm.getOrDefault(ch, 0) + 1);
+            } else {
+                hm.put(ch, i);
+            }
+        }
+    }
+}
