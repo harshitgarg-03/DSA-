ad99f7e35ed5f1b56aa294bf87b6f4d31764617a
Harshit Garg~ <gargh2120@gmail.com>
2026-08-03 10:59:03 +0530

    two sum

--- Hashing/Two_sum.java
+++ Hashing/Two_sum.java
@@ -1,23 +1,44 @@
 
+import java.util.HashMap;
+
 public class Two_sum {
-    static class info implements Comparable<info> {
-        int idx;
-        int val;
-
-        public info(int idx, int val) {
-            this.idx = idx;
-            this.val = val;
-        }
-
-        public int compareTo(info that) {
-            return this.val - that.val;
-        }
-    }
+    /*
+     * static class info implements Comparable<info> {
+     * int idx;
+     * int val;
+     * 
+     * public info(int idx, int val) {
+     * this.idx = idx;
+     * this.val = val;
+     * }
+     * 
+     * public int compareTo(info that) {
+     * return this.val - that.val;
+     * }
+     * }
+     */
 
     public static void main(String[] args) {
         int arr[] = { 2, 7, 11, 15 };
         int k = 9;
 
-        System.out.println("hello ");
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            hm.put(arr[i], i);
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            int t = k-arr[i];
+
+            if(hm.containsKey(t)){
+                int otheridx = hm.get(t);
+                if(i != otheridx){
+                    System.out.println("two idx's are :: " + i + " "+ hm.get(t));
+                    break;
+                }
+            }
+        }
+
+        // System.out.println(hm);
     }
 }
