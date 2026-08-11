4019f64750b61bb5ea7402614c91c094815f8731
Harshit Garg~ <gargh2120@gmail.com>
2026-08-11 11:30:05 +0530

    remove duplicate

---
+++ Practice-Sheet/Array/1DArr/Remove_Dup.java
@@ -0,0 +1,25 @@
+
+import java.util.Arrays;
+
+public class Remove_Dup {
+
+    public static void main(String args[]) {
+        int nums[] = { 0, 0, 1, 1, 1, 1, 2, 3, 3 };
+
+        int k = 1;
+        int ct = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if(nums[i] == nums[i-1] && ct < 2){
+                nums[k++] = nums[i];
+                ct++;
+            }
+            else if (nums[i] != nums[i-1]) {
+                nums[k++] = nums[i];
+                ct = 1;
+            }
+        }
+
+        System.out.println(Arrays.toString(nums));
+    }
+}
